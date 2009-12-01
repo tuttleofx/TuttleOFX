cd5d8ff9815b05a84c91ef93e11c1ca492d84f64
Guillaume Chatelet <clitte@miniclitte.bbteam.fr>
2009-12-01 14:45:01 +0100

    Correcting the uncrustify script to include .h files

--- uncrustify.sh
+++ uncrustify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 FILES=files_to_uncrustify_delete_me.txt
-find applications libraries plugins -type f -name "*.?pp" -o -name "*.tcc">$FILES
+find applications libraries plugins -type f -name "*.?pp" -o -name "*.tcc" -o -name "*.h">$FILES
 uncrustify -c uncrustify.cfg --no-backup --mtime -l CPP -p uncrustify.log -F $FILES
 rm -f $FILES
