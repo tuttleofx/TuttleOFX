62f63529fb8610432e820b604a5c2dd719cfaeb2
Fabien Castan <fabien.castan@free.fr>
2009-12-10 12:21:19 +0100

    MemoryCache/MemoryPool interfaces

---
+++ libraries/tuttle/src/tuttle/host/core/memory/IMemoryPool.hpp
@@ -0,0 +1,25 @@
+#ifndef _TUTTLE_HOST_CORE_IMEMORYPOOL_HPP_
+#define _TUTTLE_HOST_CORE_IMEMORYPOOL_HPP_
+
+namespace tuttle {
+namespace host {
+namespace core {
+
+typedef unsigned long long SizeInteger;
+
+class IMemoryPool
+{
+public:
+	virtual SizeInteger getMemorySizeUsed() const       = 0;
+	virtual SizeInteger getMemorySizeAllocated() const  = 0;
+	virtual SizeInteger getMemorySizeAuthorized() const = 0;
+	virtual SizeInteger clear( SizeInteger size )       = 0;
+	virtual SizeInteger clearOne()                      = 0;
+	virtual SizeInteger clearAll()                      = 0;
+};
+
+}
+}
+}
+
+#endif
