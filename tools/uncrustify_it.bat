8a80886864b623fbf81199c64c077d2cefe3a6df
Guillaume Chatelet <clitte@miniclitte.bbteam.fr>
2010-01-08 18:18:32 +0100

    adding copyright header source file via uncrustify, moving uncrustify stuffs to tools folder

---
+++ tools/uncrustify_it.bat
@@ -0,0 +1,7 @@
+REM Script out of date, do not use
+
+REM @echo off
+REM set FILES=files_to_uncrustify_delete_me.txt
+REM dir applications\*.?pp libraries\*.?pp plugins\*.?pp /s /b  > %FILES%
+REM uncrustify -c uncrustify.cfg --no-backup --mtime -l CPP -p uncrustify.log -F %FILES%
+REM del %FILES%
--- uncrustify.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-uncrustify -c uncrustify.cfg --no-backup --mtime -p uncrustify.log $(find applications libraries plugins -type f -name "*.?pp" -o -name "*.tcc" -o -name "*.h")
