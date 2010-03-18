5467f390bf884b6c48c0129eddb5b5aab6554e47
Nicolas Rondaud <nrondaud@gmail.com>
2010-03-18 12:01:44 +0100

    Bjam fixes (tests)

--- libraries/tuttle/tests/internalGraph/main.cpp
+++ libraries/tuttle/tests/internalGraph/main.cpp
@@ -27,7 +27,7 @@ BOOST_AUTO_TEST_CASE( internalGraph_add_one_vertex )
 	vDescriptor_t ADesc = graph.addVertex( A );
 
 	BOOST_CHECK_EQUAL( graph.getVertexCount(), 1 );
-	BOOST_CHECK_EQUAL( graph.instance( ADesc ).name(), string( "nodeA" ) );
+	BOOST_CHECK_EQUAL( graph.instance( ADesc ).getName(), string( "nodeA" ) );
 
 }
 
