18af853cf5328f3e138685a2382e202c232638ec
Alexandre Parent <barbak@gmail.com>
2010-04-26 19:53:29 +0200

    WIP (For the distrib functionnality) NB: Tuttle still compile.

--- libraries/tuttle/src/tuttle/plugin/ImageProcessor.hpp
+++ libraries/tuttle/src/tuttle/plugin/ImageProcessor.hpp
@@ -1,45 +1,8 @@
-#ifndef _ofxsProcessing_h_
-#define _ofxsProcessing_h_
+#ifndef _TUTTLE_PLUGIN_IMAGEPROCESSOR_HPP_
+#define _TUTTLE_PLUGIN_IMAGEPROCESSOR_HPP_
 
-/*
- * OFX Support Library, a library that skins the OFX plug-in API with C++ classes.
- * Copyright (C) 2005 The Open Effects Association Ltd
- * Author Bruno Nicoletti bruno@thefoundry.co.uk
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * Neither the name The Open Effects Association Ltd, nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * The Open Effects Association Ltd
- * 1 Wardour St
- * London W1D 6PA
- * England
- *
- *
- *
- */
-
-#include "ofxsImageEffect.h"
-#include "ofxsMultiThread.h"
+#include <ofxsImageEffect.h>
+#include <ofxsMultiThread.h>
 
 /** @file This file contains a useful base class that can be used to process images
  *
@@ -47,8 +10,8 @@
  * specific image processing algorithms. As such it does not sit in the support include lib, but in
  * its own include directory.
  */
-
-namespace OFX {
+namespace tuttle {
+namespace plugin {
 
 ////////////////////////////////////////////////////////////////////////////////
 // base class to process images with
@@ -118,5 +81,6 @@ public:
 
 };
 
-};
+}
+}
 #endif
