a6d7d2b26678418b3be48621a1f20291c96a47d3
Alexandre Parent <barbak@gmail.com>
2010-04-27 13:59:09 +0200

    Refact FFMpeg

---
+++ plugins/image/io/FFMpeg/src/mainEntry.cpp
@@ -0,0 +1,19 @@
+#define OFXPLUGIN_VERSION_MAJOR 1
+#define OFXPLUGIN_VERSION_MINOR 0
+
+#include <tuttle/plugin/Plugin.hpp>
+#include "reader/FFMpegReaderPluginFactory.hpp"
+#include "writer/FFMpegWriterPluginFactory.hpp"
+
+namespace OFX
+{
+namespace Plugin
+{
+void getPluginIDs( OFX::PluginFactoryArray& ids )
+{
+	mAppendPluginFactory( ids, tuttle::plugin::ffmpeg::reader::FFMpegReaderPluginFactory, "fr.tuttle.ffmpegreader" );
+	mAppendPluginFactory( ids, tuttle::plugin::ffmpeg::writer::FFMpegWriterPluginFactory, "fr.tuttle.ffmpegwriter" );
+}
+
+}
+}
