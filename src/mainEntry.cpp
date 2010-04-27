826bd55f723b44abe4ceecdf401ad8773bdc447d
Alexandre Parent <barbak@gmail.com>
2010-04-27 14:06:24 +0200

    Minor

--- src/mainEntry.cpp
+++ src/mainEntry.cpp
@@ -1,3 +1,6 @@
+#define OFXPLUGIN_VERSION_MAJOR 1
+#define OFXPLUGIN_VERSION_MINOR 0
+
 #include <tuttle/plugin/Plugin.hpp>
 #include "reader/Jpeg2000ReaderPluginFactory.hpp"
 #include "writer/Jpeg2000WriterPluginFactory.hpp"
@@ -7,15 +10,8 @@ namespace Plugin {
 
 void getPluginIDs( OFX::PluginFactoryArray& ids )
 {
-	//mAppendPluginFactory( ids, tuttle::plugin::pushPixel::PushPixelPluginFactory, "fr.tuttle.duranduboi.pushpixel" );
-	mAppendPluginFactoryVersions( ids,
-								  tuttle::plugin::jpeg2000::reader::Jpeg2000ReaderPluginFactory,
-								  "fr.tuttle.duranduboi.jpeg2000reader",
-								  1, 0 );
-	mAppendPluginFactoryVersions( ids,
-								  tuttle::plugin::jpeg2000::writer::Jpeg2000WriterPluginFactory,
-								  "fr.tuttle.duranduboi.jpeg2000writer",
-								  1, 0 );
+	mAppendPluginFactory( ids, tuttle::plugin::jpeg2000::reader::Jpeg2000ReaderPluginFactory, "fr.tuttle.duranduboi.jpeg2000reader" );
+	mAppendPluginFactoryVersions( ids, tuttle::plugin::jpeg2000::writer::Jpeg2000WriterPluginFactory, "fr.tuttle.duranduboi.jpeg2000writer" );
 }
 
 }
