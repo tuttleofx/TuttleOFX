a9b008d0a6102a7737c99024da829b926f235b94
Alexandre Parent <barbak@gmail.com>
2010-04-27 13:56:10 +0200

    Refact Exr

---
+++ plugins/image/io/Exr/src/mainEntry.cpp
@@ -0,0 +1,19 @@
+#define OFXPLUGIN_VERSION_MAJOR 1
+#define OFXPLUGIN_VERSION_MINOR 0
+
+#include <tuttle/plugin/Plugin.hpp>
+#include "reader/EXRReaderPluginFactory.hpp"
+#include "writer/EXRWriterPluginFactory.hpp"
+
+namespace OFX
+{
+namespace Plugin
+{
+void getPluginIDs( OFX::PluginFactoryArray& ids )
+{
+	mAppendPluginFactory( ids, tuttle::plugin::exr::reader::EXRReaderPluginFactory, "fr.tuttle.exrreader" );
+	mAppendPluginFactory( ids, tuttle::plugin::exr::writer::EXRWriterPluginFactory, "fr.tuttle.exrwriter" );
+}
+
+}
+}
