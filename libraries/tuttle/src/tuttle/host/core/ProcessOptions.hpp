06d14c15d56defad6b85b2e15715ffffd42f2730
Fabien Castan <fabien.castan@free.fr>
2010-08-06 20:40:41 +0200

    new struct for process infos

--- libraries/tuttle/src/tuttle/host/core/ProcessOptions.hpp
+++ libraries/tuttle/src/tuttle/host/core/ProcessOptions.hpp
@@ -10,6 +10,25 @@ namespace tuttle {
 namespace host {
 namespace core {
 
+struct ProcessInfos
+{
+	ProcessInfos()
+	: _memory(0)
+	, _nodes(0)
+	{}
+
+	std::size_t _memory;
+	std::size_t _nodes;
+
+	ProcessInfos& operator+=( const ProcessInfos& p )
+	{
+		_memory += p._memory;
+		_nodes += p._nodes;
+		return *this;
+	}
+};
+
+
 class ProcessOptions
 {
 public:
@@ -21,8 +40,11 @@ public:
 	OfxTime _startFrame;
 	OfxTime _endFrame;
 	OfxTime _step;
-	bool _interactive;
+	bool    _interactive;
 	OfxTime _time;
+
+	ProcessInfos _localUsage;
+	ProcessInfos _globalUsage;
 
 	// imageEffect specific options
 	std::string _field;
