63827b60e3c7d2eda02def69acc8157ca816b640
Fabien Castan <fabien.castan@free.fr>
2010-08-18 16:53:46 +0200

    rewritting sequence detection

--- libraries/tuttle/src/tuttle/common/clip/Sequence.cpp
+++ libraries/tuttle/src/tuttle/common/clip/Sequence.cpp
@@ -1,372 +1,307 @@
 #include "Sequence.hpp"
 
 #include <boost/filesystem/operations.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/regex.hpp>
+#include <boost/foreach.hpp>
+#include <boost/bind.hpp>
 
 #include <limits>
-#include <string>
-#include <sstream>
-#include <iostream>
 #include <iomanip>
-#include <vector>
 #include <list>
 
 namespace tuttle {
 namespace common {
 
-using namespace boost::filesystem;
-using namespace boost;
-using namespace std;
+namespace fs = boost::filesystem;
 
-
-namespace {
-
-struct FileDesc
+Sequence::Sequence()
+: _strictPadding(0)
+, _padding(0)
+, _step(1)
+, _firstTime(0)
+, _lastTime(0)
+, _nbFrames(0)
 {
-	FileDesc(const boost::filesystem::path & filepath, const boost::regex & regex)
-	{
-		boost::cmatch matches;
-		std::string filename = filepath.filename();
-		boost::regex_match( filename.c_str(), matches, regex );
-
-		_prefix = std::string( matches[1].first, matches[1].second );
-		_postfix = std::string( matches[3].first, matches[3].second );
-		_filenameLong = filepath.filename();
-		_sortingFilename = filepath.parent_path().string() + "/" + _prefix + _postfix;
-
-		std::string num = std::string( matches[2].first, matches[2].second );
-		std::istringstream strmLastNum(num);
-		strmLastNum >> _num;
-		_numFill = (size_t)num.length();
-	}
-	std::string _prefix;
-	std::string _postfix;
-	std::string _filenameLong;
-	std::string _sortingFilename;
-	size_t _num;
-	size_t _numFill;
-};
-
-struct FileSort
-{
-	bool operator() (const FileDesc &c1, const FileDesc &c2)
-	{
-		if (c1._sortingFilename != c2._sortingFilename)
-		{
-			return c1._sortingFilename < c2._sortingFilename;
-		}
-		else
-		{
-			return c1._num < c2._num;
-		}
-	}
-};
-
 }
 
-
-///@todo: windows filename check
-Sequence::Sequence(const path & directory, const std::string & pattern,
-                                 const bool dirbase /* = false */, const size_t start /*= 0*/, const size_t step /* = 1 */)
-: _numFill(0), _step(0), _first(0), _last(0)
+Sequence::Sequence( const boost::filesystem::path& seqPath )
+: _strictPadding(0)
+, _padding(0)
+, _step(1)
+, _firstTime(0)
+, _lastTime(0)
+, _nbFrames(0)
 {
-	path p;
-	if (directory.string().length() >= 1 && directory.string()[directory.string().length()-1] != '/')
-	{
-		p = directory.string() + "/";
-	}
-	else
-	{
-		p = directory;
-	}
-	reset( p.string() + pattern, dirbase, start, step);
-}
-
-Sequence::Sequence(const boost::filesystem::path & filepath,
-                                 const bool dirbase /* = false */, const size_t start /*= 0*/, const size_t step /* = 1 */)
-: _numFill(0), _step(0), _first(0), _last(0)
-{
-	reset(filepath, dirbase, start, step);
+	init( seqPath );
 }
 
 Sequence::~Sequence()
 {
 }
 
-void Sequence::reset(path filepath, const bool dirbase, const size_t start, const size_t step)
+/**
+ * @brief Extract step from a sorted list of time values.
+ */
+std::size_t Sequence::extractStep( const std::list<Time>& times )
 {
-	_numFill = 0;
-	_step = step;
-	_first = start;
-	_last = start;
-	_matchList.clear();
-	_pattern = "";
-
-	if( filepath.empty() )
+	// init with first step
+	std::size_t step = *(++times.begin()) - times.front(); // times[1] - times[0]
+	// to get the step we use so smallest step, others are considered as missing frames
+	// no check if multiple steps inside a sequence... ignored case.
+	for( std::list<Time>::const_iterator itA = times.begin(), itB = ++times.begin(), itEnd = times.end();
+	     itB != itEnd;
+	     ++ itA, ++itB )
 	{
+		std::size_t localStep = *itB - *itA;
+		step = std::min( step, localStep );
+	}
+	return step;
+}
+
+bool Sequence::isInSequence( const std::string& filename, Time& time )
+{
+	std::size_t min = _prefix.size() + _suffix.size();
+	if( filename.size() <= min )
+		return false;
+
+	if( filename.substr(0,_prefix.size()) != _prefix ||
+		filename.substr(filename.size()-_suffix.size(),_suffix.size()) != _suffix )
+		return false;
+
+	try
+	{
+		time = boost::lexical_cast<Time>(
+			filename.substr( _prefix.size(),
+						     filename.size()-_suffix.size()-_prefix.size() ) );
+	}
+	catch(...)
+	{
+		return false;
+	}
+	return true;
+}
+
+void Sequence::init( const boost::filesystem::path& directory, const std::string& pattern )
+{
+	clear();
+	initFromPattern( pattern );
+
+	if( ! exists( directory ) )
 		return;
-	}
-	if( filepath.parent_path().empty() )
+
+	std::list<Time> allFrames;
+
+	fs::directory_iterator itEnd;
+	for( fs::directory_iterator iter(directory); iter != itEnd; ++iter )
 	{
-		filepath = boost::filesystem::current_path().string() + "/" + filepath.string();
+		// skip directories
+		if( fs::is_directory( iter->status() ) )
+			continue;
+		
+		Time time;
+		if( isInSequence( iter->filename(), time ) )
+		{
+			allFrames.push_back( time );
+		}
 	}
-
-	// Default
-	_pattern = filepath.filename();
-
-	// max number of digits for size_t
-	static const size_t max = std::numeric_limits<size_t>::digits10+1;
-
-	const regex esc("[\\^\\.\\$\\|\\(\\)\\[\\]\\*\\+\\?\\/\\\\]");
-	string directory = filepath.parent_path().string() + "/";
-
-	string escPattern = regex_replace(filepath.filename(), esc, "\\\\\\1&", boost::match_default | boost::format_sed);
-	_fillCar = "0";
-	string fn = filepath.filename();
 	
-	// Common style with padding (eg. "filename_####.png") or without padding (eg. "filename_@.png" )
-	if( regex_match(fn, regex(".*?[_\\.]?\\[?(#+|@+)\\]?\\..*")) )
+	if( allFrames.size() < 2 ) // not really a sequence...
 	{
-		// Parse to get filling information
-		cmatch matches;
-		// Detect filename sequence based
-		regex_match( fn.c_str(), matches, regex("(.*?[_\\.]?)\\[?(#+|@+)\\]?(\\.?.*)") );
-		_fillCar = "0";
-		_prefixDir   = directory;
-		_prefixFile  = string( matches[1].first, matches[1].second );
-		_numFill     = (string( matches[2].first, matches[2].second )).length();
-		_postfixFile = string( matches[3].first, matches[3].second );
-
-		if( dirbase )
+		if( allFrames.size() == 1 )
 		{
-			// Parse filenames sequence
-			regex rx = regex("(.*?[_\\\\\\.]?)(\\\\\\[)?(#+|@+)(\\\\\\])?(.*(\\\\\\.)?.*)");
-			std::ostringstream o;
-			o << "(\\1)(\\\\\\d{" << _numFill << ", " << max << "})(\\5\\)\\$";
-			_pattern = regex_replace(escPattern, rx, o.str(), boost::match_default | boost::format_sed);
+			_firstTime = _lastTime = allFrames.front();
 		}
-	}
-	// Nuke style ( eg. "filename_%04d.png" )
-	else if( regex_match(fn, regex(".*%[0-9]+d.*")) )
-	{
-		// Parse to get filling information
-		cmatch matches;
-		regex_match( fn.c_str(), matches, regex("(.*[_\\.]?)%([0-9]|\\w)?([0-9]*)d(\\.?.*)") );
-		_prefixDir  = directory;
-		_prefixFile = string( matches[1].first, matches[1].second );
-		_fillCar    = string( matches[2].first, matches[2].second );
-		istringstream strm(std::string( matches[3].first, matches[3].second ));
-		strm >> _numFill;
-		_postfixFile = std::string( matches[4].first, matches[4].second );
-
-		if (dirbase)
-		{
-			// Parse filenames sequence
-			regex rx = regex("(.*)%([0-9])([0-9])+d(.*(\\\\\\.)?.*?)");
-			std::ostringstream o;
-			o << "(\\1)(\\\\\\d{\\3, " << max << "})(\\4\\)\\$";
-			_pattern = regex_replace(escPattern, rx, o.str(), boost::match_default | boost::format_sed);
-		}
-	}
-	else
-	{
-		// Detect filename sequence based
-		boost::cmatch matches;
-		// Parse to get filling information
-		// Detect filename sequence based
-		regex_match( fn.c_str(), matches, regex("(.*[_\\.]?)(\\..*)") );
-		_prefixDir = directory;
-		_prefixFile = std::string( matches[1].first, matches[1].second );
-		_postfixFile = filepath.extension();
-		_fillCar = "";
-		_numFill = 0;
 		return;
 	}
-	if( dirbase )
-	{
-		_matchList = matchingGroups(directory, regex( _pattern ) );
-		if( _matchList.size() > 1 )
-		{
-			COUT_WARNING("Multiple sequences found in directory: " << directory << endl << "Basing on first pattern.");
-		}
-		else if( _matchList.size() == 0 )
-		{
-//			COUT_WARNING("No pattern was found, disabling file directory pattern searching");
-			return;
-		}
 
-		FilenamesGroup g = _matchList.front();
-		_prefixDir = directory;
-		_prefixFile = g._prefix;
-		_postfixFile = g._postfix;
-		_step = g._step;
-		_first = g._first;
-		_last = g._last;
-		_numFill = g._numFill;
-	}
+	allFrames.sort();
+
+	_step = extractStep( allFrames );
+
+	_firstTime = allFrames.front();
+	_lastTime = allFrames.back();
+	_nbFrames = allFrames.size();
 }
 
-const std::string Sequence::getFirstFilename(const ssize_t nGroup /* = -1 */) const
+
+/**
+ * @brief This function creates a regex from the pattern,
+ *        and init internal values.
+ */
+void Sequence::initFromPattern( const std::string& pattern )
 {
-	return getFilenameAt(double(_first), nGroup);
-}
+	// common used pattern with # or @
+	static const boost::regex standardRegex(
+		"(.*?)"             // anything but without priority
+	    "\\[?"              // if pattern is myimage[####].jpg, don't capture []
+		"(#+|@+)"           // we capture all # or @
+		"\\]?"              // possible end of []
+		"(.*?)"             // anything
+		);
+	// C style pattern
+	static const boost::regex cstyleRegex(
+		"(.*?)"             // anything but without priority
+	    "\\[?"              // if pattern is myimage[%04d].jpg, don't capture []
+		"%([0-9]*)d"        // we capture the padding value (eg. myimage%04d.jpg)
+		"\\]?"              // possible end of []
+		"(.*?)"             // anything
+		);
+	// image name
+	static const boost::regex frameRegex(
+		"(.*?"              // anything but without priority
+		"[_\\.]?)"          // if multiple numbers, the number surround with . _ get priority (eg. seq1shot04myimage.0123.jpg -> 0123)
+	    "\\[?"              // if pattern is myimage[0001].jpg, don't capture []
+		"([\\-\\+]?[0-9]+)" // one frame number, can be positive or negative values ( -0012 or +0012 or 0012)
+		"\\]?"              // possible end of []
+		"([_\\.]?"          // if multiple numbers, the number surround with . _ get priority (eg. seq1shot04myimage.0123.jpg -> 0123)
+	    ".*\\.?"            //
+		".*?)"              // anything
+		);
 
-const std::string Sequence::getFilenameAt(const OfxTime time, const ssize_t nGroup /* = -1 */) const
-{
-	if (_pattern == "")
+	boost::cmatch matches;
+	if( regex_match( pattern.c_str(), matches, standardRegex ) )
 	{
-		return "";
+		COUT_INFOS;
+		std::string paddingStr( matches[2].first, matches[2].second );
+		COUT_VAR(paddingStr);
+		_padding = paddingStr.size();
+		_strictPadding = ( paddingStr[0] == '#' );
 	}
-	// Check for fractionnal part
-	if( time != std::floor(time) )
+	else if( regex_match( pattern.c_str(), matches, cstyleRegex ) )
 	{
-		COUT_WARNING("Warning ! Passing fractionnal time value. Frame will be overwritten.");
+		COUT_INFOS;
+		std::string paddingStr( matches[2].first, matches[2].second );
+		COUT_VAR(paddingStr);
+		_padding = paddingStr.size() == 0 ? 0 : boost::lexical_cast<std::size_t>( paddingStr ); // if no padding value: %d -> padding = 0
+		_strictPadding = false;
 	}
-
-	// Scale & translate
-	ssize_t t = ssize_t(time) * _step;
-	std::ostringstream o;
-
-	if (nGroup >= 0 && nGroup < (ssize_t)_matchList.size())
+	else if( regex_match( pattern.c_str(), matches, frameRegex ) )
 	{
-		o << _prefixDir << _prefixFile << nGroup << "_";
-		if (_numFill)
-		{
-			o << setw(_numFill) << setfill(_fillCar[0]) << t;
-		}
-		else
-		{
-			if (_fillCar.length())
-			{
-				o << t;
-			}
-		}
-		o << _postfixFile;
+		COUT_INFOS;
+		std::string frame( matches[2].first, matches[2].second );
+		COUT_VAR(frame);
+		Time t = boost::lexical_cast<Time>( frame );
+		_padding = frame.size();
+		// init begin/end to the pattern frame value
+		_firstTime = _lastTime = t;
+		_strictPadding = false;
 	}
 	else
 	{
-		o << _prefixDir << _prefixFile;
-		if (_numFill)
-		{
-			o << setw(_numFill) << setfill(_fillCar[0]) << t;
-		}
-		else
-		{
-			if (_fillCar.length())
-			{
-				o << t;
-			}
-		}
-		o << _postfixFile;
+		BOOST_THROW_EXCEPTION( std::logic_error("Unrecognized pattern.") );
 	}
-	return o.str();
+	COUT_INFOS;
+	_prefix = std::string( matches[1].first, matches[1].second );
+	_suffix = std::string( matches[3].first, matches[3].second );
 }
 
-const size_t Sequence::numGroups() const
+
+std::vector<Sequence> sequencesInDir( const boost::filesystem::path& directory )
 {
-	return _matchList.size();
-}
+	typedef Sequence::Time Time;
+	static const boost::regex expression(
+		"(.*"              // anything but without priority
+		"[_\\.]?)"          // if multiple numbers, the number surround with . _ get priority (eg. seq1shot04myimage.0123.jpg -> 0123)
+		"([\\-\\+]?[0-9]+)" // one frame number, can be positive or negative values ( -0012 or +0012 or 0012)
+		"([_\\.]?"          // if multiple numbers, the number surround with . _ get priority (eg. seq1shot04myimage.0123.jpg -> 0123)
+	    ".*?\\.?"            //
+		".*?)"              // anything
+		);
+	std::vector<Sequence> output;
 
-const OfxRangeI Sequence::getRange(const ssize_t nGroup /* = -1 */) const
-{
-	OfxRangeI range;
-	if (nGroup >= 0 && nGroup < (ssize_t)_matchList.size())
-	{
-		range.min = _matchList[nGroup]._first;
-		// Unscaled end time offset
-		range.max = _matchList[nGroup]._last / _matchList[nGroup]._step;
-	}
-	else
-	{
-		range.min = _first;
-		// Unscaled end time offset
-		range.max = _last / _step;
-	}
-	return range;
-}
+	if( ! exists( directory ) )
+		return output;
 
-vector< FilenamesGroup > Sequence::matchingGroups(const boost::filesystem::path & directory, const boost::regex & regex)
-{
-	vector< FilenamesGroup > groups;
-	vector<string> l;
-	list<FileDesc> filelist;
-	string lastPrefix, lastPostfix;
-	size_t lastNum = 0;
-	size_t lastStep = 0;
-	size_t first = 0;
-	size_t lastNumFill = 0;
-	if( exists( directory ) )
+	typedef std::pair<Sequence, std::list<Time> > SeqTmp;
+	typedef std::map<std::string, SeqTmp> SeqTmpMap;
+	SeqTmpMap sequences;
+
+	fs::directory_iterator itEnd;
+	for( fs::directory_iterator iter(directory); iter != itEnd; ++iter )
 	{
-		// Get directory files list
-		directory_iterator end;
-		for( directory_iterator iter(directory); iter != end; ++iter )
+		// skip directories
+		if( fs::is_directory( iter->status() ) )
+			continue;
+
+		boost::cmatch matches;
+		if( boost::regex_match( iter->filename().c_str(), matches, expression ) )
 		{
-			// Check if file
-			if ( !is_directory( *iter ) )
+			std::string prefix( matches[1].first, matches[1].second );
+			std::string timeStr( matches[2].first, matches[2].second );
+			std::string suffix( matches[3].first, matches[3].second );
+			std::string id( prefix+suffix );
+			COUT_VAR( iter->filename() );
+			COUT_VAR( prefix );
+			COUT_VAR( suffix );
+			COUT_VAR( timeStr );
+			Time time = boost::lexical_cast<Time>( timeStr );
+
+			SeqTmpMap::iterator it = sequences.find( id );
+			if( it == sequences.end() )
 			{
-				if ( regex_match(iter->path().filename(), regex) )
-				{
-					filelist.push_back( FileDesc(iter->path().external_file_string(), regex) );
-				}
+				Sequence s;
+				s._directory = directory;
+				s._prefix = prefix;
+				s._suffix = suffix;
+				s._padding = timeStr.size();
+				s._firstTime = time;
+				s._lastTime = time;
+				s._nbFrames = 1;
+				sequences[id] = SeqTmp( s, std::list<Time>() );
+			}
+			else
+			{
+				it->second.second.push_back( time );
+				// no check for different padding... ignored case.
 			}
 		}
-		// Sort by groups
-		filelist.sort( FileSort() );
+	}
 
-		for(list<FileDesc>::const_iterator it = filelist.begin(); it != filelist.end(); ++it)
-		{
-			if (l.size() == 0)
-			{
-				first = it->_num;
-			}
-			// Detect group change
-			else if ( l.size() && (lastPrefix != it->_prefix || lastPostfix != it->_postfix) )
-			{
-				FilenamesGroup g;
-				g._filenames = l;
-				g._first = first;
-				g._last = lastNum;
-				g._step = lastStep ? lastStep : 1;
-				g._prefix = it->_prefix;
-				g._postfix = it->_postfix;
-				g._numFill = it->_numFill;
-				groups.push_back(g);
-				l.clear();
-				lastStep = 0;
-				lastNum = 0;
-			}
-			else if ( l.size() && lastStep == 0 )
-			{
-				lastStep = (size_t)std::abs((int)(it->_num - lastNum));
-			}
-			else if ( l.size() )
-			{
-				if (std::abs((int)(it->_num - lastNum)) != (int)lastStep)
-				{
-					COUT_WARNING("Multiple steps in filename pattern (Image not found ?): " << it->_filenameLong);
-				}
-			}
-			lastPrefix = it->_prefix;
-			lastPostfix = it->_postfix;
-			lastNum = it->_num;
-			lastNumFill = it->_numFill;
-			l.push_back(it->_filenameLong);
-		}
-	}
-	// Sort filenames
-	if (l.size())
+	BOOST_FOREACH( SeqTmpMap::value_type p, sequences )
 	{
-		FilenamesGroup g;
-		g._filenames = l;
-		g._first = first;
-		g._last = lastNum;
-		g._step = lastStep ? lastStep : 1;
-		g._prefix = lastPrefix;
-		g._postfix = lastPostfix;
-		g._numFill = lastNumFill;
-		groups.push_back(g);
+		SeqTmp& sTmp = p.second;
+		Sequence& s = sTmp.first;
+		std::list<Time>& t = sTmp.second;
+		t.sort();
+		s._firstTime = t.front();
+		s._lastTime = t.back();
+		s._nbFrames = t.size();
+		s._step = Sequence::extractStep( t );
 	}
-	return groups;
+
+	output.reserve( sequences.size() );
+	
+	std::transform( sequences.begin(), sequences.end(), std::back_inserter(output),
+				 boost::bind(&SeqTmpMap::value_type::second_type::first,
+						  boost::bind(&SeqTmpMap::value_type::second, _1) ) );
+
+	return output;
 }
+
+
+std::vector<Sequence> sequencesInDir( const boost::filesystem::path& directory, const boost::regex& filter )
+{
+	return sequencesInDir( directory );
+}
+
+std::ostream& operator<<( std::ostream& os, const Sequence& v )
+{
+	os << "dir:" << v.getDirectory() << std::endl;
+	os << "first file:" << v.getFirstFilename() << std::endl;
+	os << "last file:" << v.getLastFilename() << std::endl;
+	os << "step:" << v.getStep() << std::endl;
+	os << "first time:" << v.getFirstTime() << std::endl;
+	os << "last time:" << v.getLastTime() << std::endl;
+	os << "padding:" << v.getPadding() << std::endl;
+	os << "strict padding:" << v.isStrictPadding() << std::endl;
+	os << "has missing frames:" << v.hasMissingFrames() << std::endl;
+	os << "identification:" << v.getIdentification() << std::endl;
+	os << "prefix:" << v.getPrefix() << std::endl;
+	os << "suffix:" << v.getSuffix() << std::endl;
+	return os;
+}
+
 
 }
 }
--- libraries/tuttle/src/tuttle/common/clip/Sequence.hpp
+++ libraries/tuttle/src/tuttle/common/clip/Sequence.hpp
@@ -8,26 +8,16 @@
 #include <boost/regex.hpp>
 #include <boost/filesystem.hpp>
 
-#include <sstream>
-#include <string>
-#include <vector>
 #include <cmath>
-#include <iostream>
+#include <limits>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+#include <cstddef>
 
 namespace tuttle {
 namespace common {
-
-struct FilenamesGroup
-{
-	std::vector< std::string > _filenames;
-	std::string _prefix;
-	std::string _postfix;
-	size_t _first;
-	size_t _last;
-	size_t _step;
-	size_t _numFill;
-	std::string _fillCar;
-};
 
 /**
  * @brief A sequence of numeroted files.
@@ -35,41 +25,108 @@ struct FilenamesGroup
 class Sequence
 {
 public:
-	Sequence(): _numFill(0), _step(0), _first(0), _last(0) {}
-	Sequence(const boost::filesystem::path& directory, const std::string & pattern, const bool dirbase = false, const size_t start = 0, const size_t step = 1 );
-	Sequence(const boost::filesystem::path& directory, const bool dirbase = false, const size_t start = 0, const size_t step = 1 );
+	typedef Sequence This;
+	typedef ssize_t Time;
+	Sequence();
+	/**
+	 * @param[in] file: a real file name from the sequence (eg. "/custom/dir/myImages.0010.jpg"),
+	 *                  or a sequence identifier (eg. "/custom/dir/myImages.####.jpg")
+	 */
+	Sequence( const boost::filesystem::path& seqPath );
+	Sequence( const Sequence& v ) { operator=(v); }
 	virtual ~Sequence();
 
-	void  reset(boost::filesystem::path filepath, const bool dirbase = false, const size_t start = 0, const size_t step = 1);
-	const std::string getFirstFilename(const ssize_t nGroup = -1) const;
-	const std::string getNextFilename(const ssize_t nGroup = -1);
-	const std::string getFilenameAt(const OfxTime time, const ssize_t nGroup = -1) const;
-	const size_t numGroups() const;
-	const OfxRangeI getRange(const ssize_t nGroup = -1) const;
-	inline std::size_t step() const;
+	void init( const boost::filesystem::path& directory, const std::string& pattern );
+	inline void init( const boost::filesystem::path& seqPath );
+	
+	inline boost::filesystem::path getDirectory() const { return _directory; }
+	inline boost::filesystem::path getAbsoluteFilePathAt( const Time time ) const;
+	inline std::string getFilenameAt( const Time time ) const;
+	inline boost::filesystem::path getFirstFilename() const { return getFilenameAt( getFirstTime() ); }
+	inline boost::filesystem::path getLastFilename() const { return getFilenameAt( getLastTime() ); }
+
+	inline std::pair<Time, Time> getRange() const { return std::pair<Time, Time>( getFirstTime(), getLastTime() ); }
+	inline std::size_t getStep() const { return _step; }
+	inline Time getFirstTime() const { return _firstTime; }
+	inline Time getLastTime() const { return _lastTime; }
+	inline std::size_t getPadding() const { return _padding; }
+	inline bool isStrictPadding() const { return _strictPadding; }
+	inline bool hasMissingFrames() const { return (_nbFrames / _step) != static_cast<std::size_t>(getLastTime()-getFirstTime()); }
+	/// @brief filename without frame number
+	inline std::string getIdentification() const { return _prefix + _suffix; }
+	inline std::string getPrefix() const { return _prefix; }
+	inline std::string getSuffix() const { return _suffix; }
+
+	/**
+	 * @brief Check if filename is inside the sequence and return it's time value.
+	 * @param[out] time: the time extract from the filename (only if contained in the sequence)
+	 * @return if the filename is contained inside the sequence
+	 */
+	bool isInSequence( const std::string& filename, Time& time );
 
 protected:
-	std::vector< FilenamesGroup > matchingGroups(const boost::filesystem::path & directory, const boost::regex & regex);
+	inline void clear();
+	void initFromPattern( const std::string& pattern );
+	static std::size_t extractStep( const std::list<Time>& times );
+
+private:
+	friend std::ostream& operator<<( std::ostream& os, const This& v );
+	friend std::vector<Sequence> sequencesInDir( const boost::filesystem::path& directory );
 
 protected:
-	boost::filesystem::path _path;					///< Path
-	std::string _pattern;							///< Current pattern
-	boost::regex _regexp;							///< Internal pattern regexp
-	std::vector< FilenamesGroup > _matchList;		///< Currently unused, but usefull to automatically get a pattern from a directory
-	std::string _prefixDir;							///< Store directory prefix
-	std::string _prefixFile;						///< Store filename prefix
-	std::string _postfixFile;						///< Store postfix
-	std::string _fillCar;							///< Filling character
-	size_t _numFill;								///< Number of filling char
-	std::size_t _step;								///< Step
-	std::size_t _first;								///< Starting num
-	std::size_t _last;								///< Ending num
+	boost::filesystem::path _directory;
+
+	std::string _prefix;         ///< filename prefix
+	std::string _suffix;        ///< filename suffix
+
+	bool _strictPadding;         ///< 
+	std::size_t _padding;        ///< padding, no padding if 0, fixed padding otherwise
+	std::size_t _step;           ///< step
+	Time _firstTime;             ///< begin time
+	Time _lastTime;              ///< end time
+	std::size_t _nbFrames;       ///< number of frames
+
+	static const char _fillCar = '0'; ///< Filling character
 };
 
-inline std::size_t Sequence::step() const
+
+std::vector<Sequence> sequencesInDir( const boost::filesystem::path& directory );
+std::vector<Sequence> sequencesInDir( const boost::filesystem::path& directory, const boost::regex& filter );
+
+
+inline std::string Sequence::getFilenameAt( const Time time ) const
 {
-	return _step;
+	std::ostringstream o;
+	o << _prefix << std::setw(_padding) << std::setfill(_fillCar) << time << _suffix;
+	return o.str();
 }
+
+inline boost::filesystem::path Sequence::getAbsoluteFilePathAt( const Time time ) const
+{
+	return _directory / getFilenameAt( time );
+}
+
+inline void Sequence::init( const boost::filesystem::path& seqPath )
+{
+	boost::filesystem::path dir = seqPath.parent_path();
+	if( dir.empty() ) // relative path
+		dir = boost::filesystem::current_path();
+	this->init( dir, seqPath.filename() );
+}
+
+inline void Sequence::clear()
+{
+	_directory.clear();
+	_prefix.clear();
+	_suffix.clear();
+	_padding = 0;
+	_step = 1;
+	_firstTime = 0;
+	_lastTime = 0;
+	_nbFrames = 0;
+}
+
+
 
 }
 }
