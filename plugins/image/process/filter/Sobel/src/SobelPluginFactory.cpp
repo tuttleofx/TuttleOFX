f041fc33aec740efb4748e2dcc7b468545ea147f
Fabien Castan <fabien.castan@free.fr>
2010-10-26 16:12:36 +0200

    add Sobel filter plugin

---
+++ plugins/image/process/filter/Sobel/src/SobelPluginFactory.cpp
@@ -0,0 +1,102 @@
+#include "SobelPluginFactory.hpp"
+#include "SobelPlugin.hpp"
+#include "SobelDefinitions.hpp"
+
+#include <tuttle/plugin/ImageGilProcessor.hpp>
+#include <tuttle/plugin/Progress.hpp>
+#include <tuttle/plugin/exceptions.hpp>
+
+#include <ofxsMultiThread.h>
+#include <boost/gil/gil_all.hpp>
+#include <boost/scoped_ptr.hpp>
+
+namespace tuttle {
+namespace plugin {
+namespace sobel {
+
+static const bool kSupportTiles = false;
+
+
+/**
+ * @brief Function called to describe the plugin main features.
+ * @param[in, out] desc Effect descriptor
+ */
+void SobelPluginFactory::describe( OFX::ImageEffectDescriptor& desc )
+{
+	desc.setLabels( "Sobel", "Sobel",
+		            "Sobel" );
+	desc.setPluginGrouping( "tuttle" );
+
+	// add the supported contexts, only filter at the moment
+	desc.addSupportedContext( OFX::eContextFilter );
+	desc.addSupportedContext( OFX::eContextGeneral );
+
+	// add supported pixel depths
+	desc.addSupportedBitDepth( OFX::eBitDepthUByte );
+	desc.addSupportedBitDepth( OFX::eBitDepthUShort );
+	desc.addSupportedBitDepth( OFX::eBitDepthFloat );
+
+	// plugin flags
+	desc.setSupportsTiles( kSupportTiles );
+}
+
+/**
+ * @brief Function called to describe the plugin controls and features.
+ * @param[in, out]   desc       Effect descriptor
+ * @param[in]        context    Application context
+ */
+void SobelPluginFactory::describeInContext( OFX::ImageEffectDescriptor& desc,
+                                                  OFX::EContext context )
+{
+	OFX::ClipDescriptor* srcClip = desc.defineClip( kOfxImageEffectSimpleSourceClipName );
+	srcClip->addSupportedComponent( OFX::ePixelComponentRGBA );
+	srcClip->addSupportedComponent( OFX::ePixelComponentAlpha );
+	srcClip->setSupportsTiles( kSupportTiles );
+
+	// Create the mandated output clip
+	OFX::ClipDescriptor* dstClip = desc.defineClip( kOfxImageEffectOutputClipName );
+	dstClip->addSupportedComponent( OFX::ePixelComponentRGBA );
+//	dstClip->addSupportedComponent( OFX::ePixelComponentAlpha );
+	dstClip->setSupportsTiles( kSupportTiles );
+
+	OFX::Double2DParamDescriptor* size = desc.defineDouble2DParam( kParamSize );
+	size->setLabel( "Size" );
+	size->setDefault( 3, 3 );
+	size->setRange( 0.0, 0.0, std::numeric_limits<double>::max(), std::numeric_limits<double>::max() );
+	size->setDisplayRange( 0, 0, 10, 10 );
+	size->setDoubleType( OFX::eDoubleTypeScale );
+
+	OFX::BooleanParamDescriptor* normalizedKernel = desc.defineBooleanParam( kParamNormalizedKernel );
+	normalizedKernel->setLabel( "Normalized kernel" );
+	normalizedKernel->setHint( "Use a normalized kernel to compute the gradient." );
+	normalizedKernel->setDefault( true );
+//#ifndef TUTTLE_PRODUCTION
+	normalizedKernel->setIsSecret( true );
+//#endif
+
+	OFX::ChoiceParamDescriptor* border = desc.defineChoiceParam( kParamBorder );
+	border->setLabel( "Gradient border" );
+	border->setHint( "Border method for gradient computation." );
+	border->appendOption( kParamBorderMirror );
+	border->appendOption( kParamBorderConstant );
+	border->appendOption( kParamBorderBlack );
+	border->appendOption( kParamBorderPadded );
+
+}
+
+/**
+ * @brief Function called to create a plugin effect instance
+ * @param[in] handle  Effect handle
+ * @param[in] context Application context
+ * @return  plugin instance
+ */
+OFX::ImageEffect* SobelPluginFactory::createInstance( OfxImageEffectHandle handle,
+                                                            OFX::EContext context )
+{
+	return new SobelPlugin( handle );
+}
+
+}
+}
+}
+
---
+++ plugins/image/process/filter/Sobel/src/SobelPluginFactory.hpp
@@ -0,0 +1,18 @@
+#ifndef _TUTTLE_PLUGIN_SOBELPLUGINFACTORY_HPP_
+#define _TUTTLE_PLUGIN_SOBELPLUGINFACTORY_HPP_
+
+#include <ofxsImageEffect.h>
+
+namespace tuttle {
+namespace plugin {
+namespace sobel {
+
+mDeclarePluginFactory( SobelPluginFactory, { }, { } );
+
+}
+}
+}
+
+#endif
+
+
