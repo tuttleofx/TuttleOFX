ba40e334ad97eebf1c87c21374abbae70dde1821
Fabien Castan <fabien.castan@free.fr>
2010-12-29 15:41:36 +0100

    doGilRender
    * add doGilRender2 with 2 different clip types
    * add planar argument

--- libraries/tuttle/src/tuttle/plugin/ImageEffectGilPlugin.hpp
+++ libraries/tuttle/src/tuttle/plugin/ImageEffectGilPlugin.hpp
@@ -26,18 +26,46 @@ public:
     OFX::Clip *_clipSrc; ///< Source image clip
 };
 
-template<template<class> class Process, class Plugin, class Layout, class Bits>
-inline void doGilRender( Plugin& plugin, const OFX::RenderArguments &args );
 
-template<template<class> class Process, class Plugin, class Layout>
-inline void doGilRender( Plugin& plugin, const OFX::RenderArguments &args, const OFX::EBitDepth bitDepth );
+template< template<class> class Process,
+          bool planar, class Layout, class Bits,
+          class Plugin >
+void doGilRender( Plugin& plugin, const OFX::RenderArguments& args );
 
-template<template<class> class Process, class Plugin>
-inline void doGilRender( Plugin& plugin, const OFX::RenderArguments &args, const OFX::EPixelComponent component, const OFX::EBitDepth bitDepth );
+template< template<class> class Process,
+	      bool planar, class Layout,
+          class Plugin >
+void doGilRender( Plugin& plugin, const OFX::RenderArguments& args, const OFX::EBitDepth bitDepth );
 
-template<template<class> class Process, class Plugin>
-inline void doGilRender( Plugin& plugin, const OFX::RenderArguments &args );
+template< template<class> class Process,
+	      bool planar,
+          class Plugin>
+void doGilRender( Plugin& plugin, const OFX::RenderArguments& args, const OFX::EPixelComponent components, const OFX::EBitDepth bitDepth );
 
+template< template<class> class Process,
+          class Plugin >
+void doGilRender( Plugin& plugin, const OFX::RenderArguments& args, const OFX::Clip& clip );
+
+/**
+ * @brief This render function, instanciate a Process class templated with the image type (layout and bit depth).
+ * @param[in]   args     Rendering parameters
+ */
+template< template<class> class Process,
+          class Plugin >
+void doGilRender( Plugin& plugin, const OFX::RenderArguments& args );
+
+template< template<class,class> class Process,
+          bool splanar, class SLayout, class SBits,
+		  bool dplanar, class DLayout, class DBits,
+          class Plugin >
+void doGilRender2( Plugin& plugin, const OFX::RenderArguments& args );
+
+template< template<class,class,class> class Process,
+          bool s1planar, class S1Layout, class S1Bits,
+          bool s2planar, class S2Layout, class S2Bits,
+		  bool dplanar, class DLayout, class DBits,
+          class Plugin>
+void doGilRender3( Plugin& plugin, const OFX::RenderArguments& args );
 
 }
 }
