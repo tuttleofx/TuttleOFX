e961798b94f58acf5ae8e9ddfcbb8e530b013b17
Marc-Antoine ARNAUD <mrn@mikrosimage.eu>
2011-02-15 14:36:41 +0100

    update of sam applications (using of TUTTLE_* functions)

--- applications/sam-cp/src/main.cpp
+++ applications/sam-cp/src/main.cpp
@@ -7,11 +7,11 @@
 #include <iostream>
 
 namespace bfs = boost::filesystem;
-using namespace tuttle::common;
+namespace ttl = tuttle::common;
 
-void copy_sequence( const Sequence s, const Sequence d )
+void copy_sequence( const ttl::Sequence s, const ttl::Sequence d )
 {
-	for( Sequence::Time t = s.getFirstTime(); t <= s.getLastTime(); t += s.getStep() )
+	for( ttl::Sequence::Time t = s.getFirstTime(); t <= s.getLastTime(); t += s.getStep() )
 	{
 		bfs::path sFile = s.getAbsoluteFilenameAt(t);
 		if( bfs::exists( sFile ) )
@@ -22,9 +22,9 @@ void copy_sequence( const Sequence s, const Sequence d )
 	}
 }
 
-void copy_sequence( const Sequence s, const bfs::path d )
+void copy_sequence( const ttl::Sequence s, const bfs::path d )
 {
-	for( Sequence::Time t = s.getFirstTime(); t <= s.getLastTime(); t += s.getStep() )
+	for( ttl::Sequence::Time t = s.getFirstTime(); t <= s.getLastTime(); t += s.getStep() )
 	{
 		bfs::path sFile = s.getFilenameAt(t);
 		if( bfs::exists( sFile ) )
@@ -36,50 +36,49 @@ void copy_sequence( const Sequence s, const bfs::path d )
 
 int main( int argc, char** argv )
 {
-
 	try
 	{
 		if( argc == 1 )
 		{
-			CERR( "No argument." );
+			TUTTLE_CERR( "No argument." );
 			return -1;
 		}
 		else if( argc == 2 )
 		{
-			CERR( "Where do you want to copy \"" << argv[1] << "\"?" );
+			TUTTLE_CERR( "Where do you want to copy \"" << argv[1] << "\"?" );
 			return -1;
 		}
-		std::vector<std::string> allSrc;
+		std::vector<char*> allSrc;
 		allSrc.reserve( argc - 1 );
 		for( int i = 1; i < argc - 1; ++i )
 			allSrc.push_back( argv[i] );
 		bfs::path dstPath( argv[argc-1] );
 		bool dstIsDir = bfs::is_directory( dstPath );
-		Sequence dstSeq;
-		bool dstIsSeq = dstSeq.init( dstPath, 0, 0, 1, Sequence::ePatternAll );
+		ttl::Sequence dstSeq;
+		bool dstIsSeq = dstSeq.init( dstPath, 0, 0, 1, ttl::Sequence::ePatternAll );
 
 		if( !dstIsDir && !dstIsSeq )
 		{
-			CERR( "Your destination must be a sequence or a directory." );
+			TUTTLE_CERR( "Your destination must be a sequence or a directory." );
 			return -1;
 		}
 		if( allSrc.size() > 1 && ! dstIsDir )
 		{
-			CERR( "To copy multiple sequences, your destination must be a directory." );
+			TUTTLE_CERR( "To copy multiple sequences, your destination must be a directory." );
 			return -1;
 		}
 		BOOST_FOREACH( bfs::path srcPath, allSrc )
 		{
-			Sequence srcSeq;
-			bool srcIsSeq = srcSeq.initFromDetection( srcPath, Sequence::ePatternDefault );
+			ttl::Sequence srcSeq;
+			bool srcIsSeq = srcSeq.initFromDetection( srcPath, ttl::Sequence::ePatternDefault );
 			if( ! srcIsSeq )
 			{
-				CERR( "Input is not a sequence. (" << srcPath << ")" );
+				TUTTLE_CERR( "Input is not a sequence. (" << srcPath << ")" );
 				return -1;
 			}
 			if( srcSeq.getNbFiles() == 0 )
 			{
-				CERR( "No existing file for the input sequence. (" << srcPath << ")" );
+				TUTTLE_CERR( "No existing file for the input sequence. (" << srcPath << ")" );
 				return -1;
 			}
 			if( dstIsSeq )
@@ -96,11 +95,11 @@ int main( int argc, char** argv )
 	}
 	catch( const bfs::filesystem_error& e )
 	{
-		std::cerr << e.what() << std::endl;
+		TUTTLE_CERR( e.what() );
 	}
 	catch(... )
 	{
-		std::cerr << boost::current_exception_diagnostic_information() << std::endl;
+		TUTTLE_CERR( boost::current_exception_diagnostic_information() );
 		return -1;
 	}
 	return 0;
