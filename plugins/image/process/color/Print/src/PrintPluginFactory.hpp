0e4d26e828c62b55bff97eedec9b98f94e3fa75d
Fabien Castan <fabien.castan@free.fr>
2011-02-20 01:03:26 +0100

    add new Print plugin
    Print content of the image on console.

---
+++ plugins/image/process/color/Print/src/PrintPluginFactory.hpp
@@ -0,0 +1,18 @@
+#ifndef _TUTTLE_PLUGIN_PRINTPLUGINFACTORY_HPP_
+#define _TUTTLE_PLUGIN_PRINTPLUGINFACTORY_HPP_
+
+#include <ofxsImageEffect.h>
+
+namespace tuttle {
+namespace plugin {
+namespace print {
+
+mDeclarePluginFactory( PrintPluginFactory, { }, { } );
+
+}
+}
+}
+
+#endif
+
+
