9dd96455bb9826e46faa8f0bf5100698c1e37a29
Marc-Antoine-ARNAUD <mrn@mikrosimage.eu>
2011-03-31 15:04:10 +0200

    test for auto copy OFX plugins

---
+++ 3rdParty/installTuttlePlugins.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ll -d /usr/OFX > /dev/null 2>&1
+if [ $? = 0 ]
+then
+mkdir /usr/OFX
+fi
+
+ll -d /usr/OFX/Plugins > /dev/null 2>&1
+if [ $? = 0 ]
+then
+mkdir /usr/OFX/Plugins
+fi
+
+cp -R ../dist/plugins/image/*/*/bundle/* /usr/OFX/Plugins
+cp -R ../dist/plugins/image/*/*/*/bundle/* /usr/OFX/Plugins
