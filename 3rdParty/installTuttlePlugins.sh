037b9c693580fc04c06500756dc37d043184cb1f
Marc-Antoine ARNAUD <mrn@mikrosimage.eu>
2011-04-13 18:55:16 +0200

    minor changes

--- 3rdParty/installTuttlePlugins.sh
+++ 3rdParty/installTuttlePlugins.sh
@@ -13,3 +13,4 @@ fi
 
 cp -R ../dist/plugins/image/*/*/bundle/* /usr/OFX/Plugins
 cp -R ../dist/plugins/image/*/*/*/bundle/* /usr/OFX/Plugins
+cp -R ../dist/plugins/param/*/*/bundle/* /usr/OFX/Plugins
