09e5bd7a37e1da5389370ef0102a318dbbe51937
Marc-Antoine ARNAUD <mrn@mikrosimage.eu>
2011-08-01 16:15:05 +0200

    insert modifications to compile with windows

--- 3rdParty/init.py
+++ 3rdParty/init.py
@@ -5,6 +5,8 @@ import os
 import sys
 import urllib
 import subprocess
+import tarfile
+import zipfile
 
 osname		= os.name.lower()
 sysplatform	= sys.platform.lower()
@@ -74,41 +76,48 @@ def dlProgress(count, blockSize, totalSize):
 	sys.stdout.flush()
 
 global knownExtensions
-knownExtensions = { 'tar': 'tar xf',
-                   'tar.gz': 'tar xfz',
-                   'tgz': 'tar xfz',
-                   'tar.bz2': 'tar xfj',
-                   'zip': 'unzip',
-                   'exe': '' }
+knownExtensions = {	'tar':		'tar xf',
+                    'tar.gz':	'tar xfz',
+                    'tgz':		'tar xfz',
+                    'tar.bz2':	'tar xfj',
+                    'zip':		'unzip',
+                    'exe':		'' }
+
 
 def getKnownExtensions( filename ):
 	global knownExtensions
 	return [f for f in knownExtensions.keys() if filename.endswith(f)]
-
+	
 def uncompress(filename, ext, inNewDirectory, libname):
-	global knownExtensions
-	binOptions = { 'tar' : {'directory':'--directory',},
-	               'unzip' : {'directory':'-d',},
-	             } 
-	cmdFromExtension = knownExtensions[ext]
-	if not cmdFromExtension:
-		return
-	cmd = cmdFromExtension.split()
-	bin = cmd[0]
-	if( inNewDirectory ):
-		cmd.append( binOptions[bin]['directory'] )
-		cmd.append( libname )
-	cmd.append(filename)
-	print '\n', ' '.join(cmd)
+
+	if ext == 'tar.gz' :
+		tar = tarfile.open(filename, 'r:*')
+		folder = './'
+		tar.extractall( folder )
+
+	if ext == 'tar.gz2' :
+		tar = tarfile.open(filename, 'r:*')
+		folder = './'
+		tar.extractall( folder )
+
+	if ext == 'zip' :
+		zip = zipfile.ZipFile( filename, 'r' )
+		folder = './'
+		if inNewDirectory == True : folder += filename[:-len(ext)-1]
+		zip.extractall( folder )
+
+	if ext == 'exe' :
+		fileToRun = filename+'.exe'
+		os.startfile( fileToRun )
+
 	if not os.path.exists( os.path.join( os.getcwd(), libname)):
 		os.mkdir(os.path.join( os.getcwd() ,libname))
-	p = subprocess.Popen(cmd).communicate()
+	
 	print 'uncompress and  copy', filename[:-len(ext)-1], libname, '\n'
 	copytree(filename[:-len(ext)-1], libname)
 	print 'end of uncompress\n'
 
 def getAndUncompress( libraries ):
-
 	for libname, url, inNewDirectory in libraries:
 		print '_'*80
 		print '--', libname
@@ -138,16 +147,16 @@ def getAndUncompress( libraries ):
 			print 'uncompress error (', str(e), ')'
 
 allLibs = [
-		('zlib','http://prdownloads.sourceforge.net/libpng/zlib-1.2.3.tar.gz', False) if not windows else ('zlib','http://downloads.sourceforge.net/project/libpng/zlib/1.2.3/zlib123.zip', False),
+		('zlib','http://prdownloads.sourceforge.net/libpng/zlib-1.2.3.tar.gz', False) if not windows else ('zlib','http://downloads.sourceforge.net/project/libpng/zlib/1.2.3/zlib123.zip', True),
 		('libpng', 'http://prdownloads.sourceforge.net/libpng/libpng-1.2.41.tar.gz', False) if not windows else ('libpng','http://downloads.sourceforge.net/project/libpng/libpng12/older-releases/1.2.41/lpng1241.zip', False),
 		('ilmbase', 'http://download.savannah.nongnu.org/releases/openexr/ilmbase-1.0.1.tar.gz', False),
 		('openexr', 'http://download.savannah.nongnu.org/releases/openexr/openexr-1.6.1.tar.gz', False),
 		('ctl', 'http://sourceforge.net/projects/ampasctl/files/ctl/ctl-1.4.1/ctl-1.4.1.tar.gz/download', False),
-		('boost', 'http://prdownloads.sourceforge.net/boost/boost_1_47_0.tar.bz2', False) if not windows else ('boost', 'http://www.boostpro.com/download/boost_1_47_setup.exe', False),
-		('freetype','http://prdownloads.sourceforge.net/freetype/freetype-2.4.3.tar.bz2', False) if not windows else ('freetype','http://prdownloads.sourceforge.net/gnuwin32/freetype-2.3.5-1-setup.exe', False),
-		('libraw','http://www.libraw.org/data/LibRaw-0.13.1.tar.gz', False),
+		('boost', 'http://prdownloads.sourceforge.net/boost/boost_1_46_0.tar.bz2', False) if not windows else ('boost', 'http://sourceforge.net/projects/boost/files/boost/1.46.1/boost_1_46_1.zip', False),
+		('freetype','http://prdownloads.sourceforge.net/freetype/freetype-2.4.3.tar.gz', False) if not windows else ('freetype','http://prdownloads.sourceforge.net/freetype/freetype-2.4.3.tar.gz', False),
+		('libraw','http://www.libraw.org/data/LibRaw-0.13.1.tar.gz', False) if not windows else ('libraw', 'http://www.libraw.org/data/LibRaw-0.13.5.zip', False),
 		('libcaca','http://caca.zoy.org/files/libcaca/libcaca-0.99.beta17.tar.gz', False),
-		('ImageMagick','ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.6.9-7.tar.gz', False) if not windows else ('ImageMagick','ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.6.9-7.zip',False),
+		('ImageMagick','ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.6.9-7.tar.gz', False) if not windows else ('ImageMagick','ftp://ftp.imagemagick.org/pub/ImageMagick/ImageMagick-6.7.0-9.zip',False),
 		('lcms','http://downloads.sourceforge.net/project/lcms/lcms/2.1/lcms2-2.1.tar.gz', False) if not windows else ('lcms', 'http://downloads.sourceforge.net/project/lcms/lcms/2.1/lcms2-2.1.zip',False),
 		('bzlib','http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz',False),
 		('libtiff','ftp://ftp.remotesensing.org/pub/libtiff/tiff-3.9.5.tar.gz', False) if not windows else ('libtiff','ftp://ftp.remotesensing.org/pub/libtiff/tiff-3.9.5.zip', False),
@@ -158,12 +167,61 @@ allLibs = [
 		('gvc','http://www.graphviz.org/pub/graphviz/stable/SOURCES/graphviz-2.26.3.tar.gz',False)
 	]
 
-if __name__ == "__main__":
-	if len( sys.argv ) == 1:
-		getAndUncompress( allLibs )
+
+def insertInFile( filename, linesIndexes, textToAppend ):
+	file = open( filename , 'r' )
+	fileTmp = open( 'tmp.txt' , 'w' )
+	lines = file.readlines()
+
+	nlist = list( lines )
+
+	index = 0;
+
+	for lineIndex in linesIndexes:
+		nlist.insert( lineIndex , textToAppend[index]+'\n' )
+		index+=1
+	fileTmp.writelines( nlist )
+
+	file.close()
+	fileTmp.close()
+	os.remove(filename)
+	os.rename('tmp.txt', filename)
+
+def makeModificationIfNecessaryInFile( filename, headerToInsert ):
+	print filename
+	modification = 0
+	if(os.path.exists( filename )):
+		for line in open(filename,'r'):
+			if headerToInsert in line:
+	   			modification = 1
+	   			break
+	   	if modification == 0 :
+	   		insertInFile(filename, (1, 63) , ("", headerToInsert ) )
+	   	else:
+	   		print "header is already include."
 	else:
-		libDic = {}
-		for lib in allLibs:
-			libDic[lib[0]] = lib
-		for libname in sys.argv[1:]:
-			getAndUncompress( [libDic[libname]] )
+		print "file is not present."
+
+if len( sys.argv ) == 1:
+	getAndUncompress( allLibs )
+else:
+	libDic = {}
+	for lib in allLibs:
+		libDic[lib[0]] = lib
+
+	for libname in sys.argv[1:]:
+		getAndUncompress( [libDic[libname]] )
+
+print '_'*80
+print '-'*29 + ' Files modifications  ' + '-'*29
+
+filename = "ctl/IlmCtl/CtlLex.cpp" 
+headerToInsert = "#include <cstdlib>"
+makeModificationIfNecessaryInFile(filename, headerToInsert );
+
+filename = "ctl/IlmCtlSimd/CtlSimdReg.cpp" 
+headerToInsert = "#include <cstring>"
+makeModificationIfNecessaryInFile(filename, headerToInsert );
+
+print 'End of initialisation.'
+
--- 3rdParty/makeBjamCommand.py
+++ 3rdParty/makeBjamCommand.py
@@ -8,26 +8,27 @@ import urllib
 import subprocess
 
 fileExec = "../bjam.sh"
+fileExecWindows = "../bjam.bat"
 
 def detectCPUs():
-	"""
-	Detects the number of CPUs on a system. Cribbed from pp.
-	"""
-	# Linux, Unix and MacOS:
-	if hasattr(os, "sysconf"):
-		if os.sysconf_names.has_key("SC_NPROCESSORS_ONLN"):
-			# Linux & Unix:
-			ncpus = os.sysconf("SC_NPROCESSORS_ONLN")
-			if isinstance(ncpus, int) and ncpus > 0:
-				return ncpus
-			else: # OSX:
-				return int(os.popen2("sysctl -n hw.ncpu")[1].read())
-			# Windows:
-			if os.environ.has_key("NUMBER_OF_PROCESSORS"):
-				ncpus = int(os.environ["NUMBER_OF_PROCESSORS"]);
-				if ncpus > 0:
-					return ncpus
-				return 1 # Default
+ """
+ Detects the number of CPUs on a system. Cribbed from pp.
+ """
+ # Linux, Unix and MacOS:
+ if hasattr(os, "sysconf"):
+     if os.sysconf_names.has_key("SC_NPROCESSORS_ONLN"):
+         # Linux & Unix:
+         ncpus = os.sysconf("SC_NPROCESSORS_ONLN")
+         if isinstance(ncpus, int) and ncpus > 0:
+             return ncpus
+     else: # OSX:
+         return int(os.popen2("sysctl -n hw.ncpu")[1].read())
+ # Windows:
+ if os.environ.has_key("NUMBER_OF_PROCESSORS"):
+         ncpus = int(os.environ["NUMBER_OF_PROCESSORS"]);
+         if ncpus > 0:
+             return ncpus
+ return 1 # Default
 
 osname		= os.name.lower()
 sysplatform	= sys.platform.lower()
@@ -39,22 +40,35 @@ is_64bits	= sys.maxsize > 2**32
 haveGcc		= False
 
 if linux :
-	if os.system("gcc -v 2&> /dev/null")==0 :
-		haveGcc = True
-	if os.path.exists(fileExec)==False :
-		f = open(fileExec, "w")
-		f.write("export BOOST_BUILD_PATH=`pwd`/3rdParty/boost/tools/build/v2\n" )
-		f.write("export BOOST_PATH=`pwd`/3rdParty/boost/\n" )
-		f.write("export BOOST_ROOT=`pwd`/3rdParty/boost/boost/\n" )
-		f.write("3rdParty/boost/bjam ")
-		if haveGcc :
-			f.write("--toolset=gcc --disable-icu ")
-		if is_64bits :
-			f.write("cxxflags=-fPIC address-model=64 ")
-		f.write("-j%s $* " % detectCPUs())
-		f.close()
-		os.chmod(fileExec,stat.S_IXUSR+stat.S_IRUSR+stat.S_IWUSR)
+  if os.system("gcc -v 2&> /dev/null")==0 :
+    haveGcc = True
+  if os.path.exists(fileExec)==False :
+    f = open(fileExec, "w")
+    f.write("bjam ")
+    if haveGcc :
+      f.write("--toolset=gcc --disable-icu ")
+    if is_64bits :
+      f.write("cxxflags=-fPIC address-model=64 ")
+    f.write("-j%s $* " % detectCPUs())
+    f.close()
+    os.chmod(fileExec,stat.S_IXUSR+stat.S_IRUSR+stat.S_IWUSR)
 
+
+if windows :
+  if os.path.exists(fileExecWindows)==False :
+    f = open(fileExecWindows, "w")
+    f.write("@echo off \n")
+    f.write("set BOOST_BUILD_PATH=%cd%\\3rdParty\\boost\\tools\\build\\v2 \n")
+    f.write("set BOOST_PATH=%cd%\\3rdParty\\boost \n")
+    f.write("set BOOST_ROOT=%cd%\\3rdParty\\boost\\boost \n")
+    f.write("@echo on \n")
+    f.write("3rdParty\\boost\\bjam --toolset=msvc --disable-icu ")
+    if is_64bits :
+      f.write("address-model=64 ")
+    f.write("-j%s " % detectCPUs())
+    f.write("%* " )
+    f.close()
+    os.chmod( fileExecWindows, stat.S_IXUSR + stat.S_IRUSR + stat.S_IWUSR )
 
 print "linux\t\t%s" % (linux)
 print "windows\t\t%s" % (windows)
