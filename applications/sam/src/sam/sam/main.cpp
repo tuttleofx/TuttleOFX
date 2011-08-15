2b9349255e4cbfd7e03dbfacf890e8dd6b5da0f2
Fabien Castan <fabien.castan@free.fr>
2011-08-15 21:58:39 +0200

    sam launcher in progress

--- applications/sam/src/sam/sam/main.cpp
+++ applications/sam/src/sam/sam/main.cpp
@@ -7,6 +7,88 @@
 #include <boost/foreach.hpp>
 #include <boost/throw_exception.hpp>
 
+namespace bfs = boost::filesystem;
+
+bfs::path retrieveToolFullPath( const std::string& toolName, const std::vector<bfs::path>& searchPaths )
+{
+	const std::string toolFilename( std::string("sam-") + toolName );
+	
+	BOOST_FOREACH( const bfs::path& p, searchPaths )
+	{
+		const bfs::path toolFullPath = p / toolFilename;
+		if( bfs::exists( toolFullPath ) )
+		{
+			return toolFullPath;
+		}
+	}
+
+	/// @todo exception ?
+	TUTTLE_CERR(
+		"Sam command \"" << toolName << "\" not found."
+		"\n"
+		);
+	// displayAvailableCommands();
+	exit( -1 );
+}
+
+std::vector<bfs::path> retrieveSearchPaths( const bfs::path& samDirectory )
+{
+	std::vector<bfs::path> searchPaths;
+	if( const char* env_ptr = std::getenv("SAM_PATH") )
+	{
+		const std::string env( env_ptr );
+		const std::vector<std::string> paths = boost::program_options::split_unix( env, ":;" );
+		searchPaths.insert( searchPaths.end(), paths.begin(), paths.end() );
+	}
+	
+	searchPaths.push_back( samDirectory / "sam" );
+	searchPaths.push_back( samDirectory );
+	
+	return searchPaths;
+}
+
+std::vector<bfs::path> retrieveAllSamCommands( const bfs::path& dir )
+{
+	std::vector<bfs::path> res;
+	if( ! bfs::is_directory(dir) )
+		return res;
+	
+	bfs::directory_iterator dir_iter(dir), dir_end;
+	for(;dir_iter != dir_end; ++dir_iter)
+	{
+		bfs::path cmd = *dir_iter;
+		if( boost::starts_with( cmd.filename().string(), "sam-" ) )
+		{
+			res.push_back( cmd );
+		}
+	}
+	return res;
+}
+
+std::vector<bfs::path> retrieveAllSamCommands( const std::vector<bfs::path>& dirs )
+{
+	typedef std::map<bfs::path,bfs::path> PathMap;
+	PathMap allRes;
+	BOOST_FOREACH( const bfs::path& dir, dirs )
+	{
+		std::vector<bfs::path> dirRes = retrieveAllSamCommands( dir );
+		BOOST_FOREACH( const bfs::path& cmd, dirRes )
+		{
+			if( allRes.find( cmd.filename() ) == allRes.end() )
+			{
+				allRes[cmd.filename()] = cmd;
+			}
+		}
+	}
+	std::vector<bfs::path> res;
+	res.reserve( allRes.size() );
+	BOOST_FOREACH( PathMap::const_reference p, allRes )
+	{
+		res.push_back( p.second );
+	}
+	return res;
+}
+
 int main( int argc, char** argv )
 {
 	if( argc <= 1 ) // no argument
@@ -18,9 +100,6 @@ int main( int argc, char** argv )
 		exit( -1 );
 	}
 
-	TUTTLE_TCOUT_VAR( argc );
-	TUTTLE_TCOUT_VAR( argv[0] );
-	
 	try
 	{
 		namespace bpo = boost::program_options;
@@ -40,8 +119,10 @@ int main( int argc, char** argv )
 		bpo::options_description infoOptions( "\tOptions" );
 		infoOptions.add_options()
 			("help,h"       , "show node help")
-			("version,v"    , "display version")
+			("version,v"    , "display version") // which version?
 			("commands,c"   , "show list of all available sam commands")
+			("commands-list" , "show list of all available sam commands") /// @todo: secret
+			("binaries-list" , "show list of all available sam binaries") /// @todo: secret
 		;
 		bpo::options_description all_options;
 		all_options.add(infoOptions);
@@ -66,7 +147,7 @@ int main( int argc, char** argv )
 			TUTTLE_COUT( "\tsam - A set of command line tools." );
 			TUTTLE_COUT( "" );
 			TUTTLE_COUT( "SYNOPSIS" );
-			TUTTLE_COUT( "\tsam command [options]..." );
+			TUTTLE_COUT( "\tsam COMMAND [options]..." );
 			TUTTLE_COUT( "" );
 			TUTTLE_COUT( "DESCRIPTION" );
 			TUTTLE_COUT( infoOptions );
@@ -78,15 +159,49 @@ int main( int argc, char** argv )
 			exit( 0 );
 		}
 		
-		boost::filesystem::path fullsam( argv[0] );
+		bfs::path fullsam( argv[0] );
 		if( fullsam.is_relative() )
 		{
-			fullsam = boost::filesystem::current_path() / fullsam;
+			fullsam = bfs::current_path() / fullsam;
 		}
-		boost::filesystem::path samDirectory( fullsam.parent_path() );
-		
-		TUTTLE_TCOUT_VAR( fullsam );
-		TUTTLE_TCOUT_VAR( samDirectory );
+		bfs::path samDirectory( fullsam.parent_path() );
+
+		const std::vector<bfs::path> searchPaths = retrieveSearchPaths( samDirectory );
+
+		if( sam_vm.count("commands") )
+		{
+			TUTTLE_COUT( "" );
+			TUTTLE_COUT( "COMMANDS" );
+			
+			const std::vector<bfs::path> cmds = retrieveAllSamCommands( searchPaths );
+			BOOST_FOREACH( const bfs::path& c, cmds )
+			{
+				TUTTLE_COUT( "\t" << c.filename().string().substr(4) );
+//				const int res = system( (c.string()+" --brief").c_str() );
+				TUTTLE_COUT( "" );
+			}
+			TUTTLE_COUT( "" );
+
+			exit( 0 );
+		}
+		if( sam_vm.count("binaries-list") )
+		{
+			const std::vector<bfs::path> cmds = retrieveAllSamCommands( searchPaths );
+			BOOST_FOREACH( const bfs::path& c, cmds )
+			{
+				TUTTLE_COUT( c.string() );
+			}
+			exit( 0 );
+		}
+		if( sam_vm.count("commands-list") )
+		{
+			const std::vector<bfs::path> cmds = retrieveAllSamCommands( searchPaths );
+			BOOST_FOREACH( const bfs::path& c, cmds )
+			{
+				TUTTLE_COUT( c.filename().string() );
+			}
+			exit( 0 );
+		}
 		
 		const std::string sam_tool( argv[cl_options.size() + 1] );
 		std::vector<std::string> sam_cmd;
@@ -96,50 +211,11 @@ int main( int argc, char** argv )
 			sam_cmd.push_back( s );
 		}
 		
-		if( sam_vm.count("commands") )
-		{
-			TUTTLE_COUT( "COMMANDS" );
-			/// @todo
-			exit( 0 );
-		}
-		
-		std::vector<boost::filesystem::path> searchPaths;
-		const std::string toolFilename( (std::string("sam-") + sam_tool) );
-		boost::filesystem::path tmpToolFullPath;
-		boost::filesystem::path toolFullPath;
-		if( const char* env_ptr = std::getenv("SAM_PATH") )
-		{
-			const std::string env( env_ptr );
-			const std::vector<std::string> paths = boost::program_options::split_unix( env, ":;" );
-			searchPaths.insert( searchPaths.end(), paths.begin(), paths.end() );
-		}
-		searchPaths.push_back( samDirectory / "sam" );
-		searchPaths.push_back( samDirectory );
-		
-		BOOST_FOREACH( const boost::filesystem::path& p, searchPaths )
-		{
-			const boost::filesystem::path tmpToolFullPath = p / toolFilename;
-			if( boost::filesystem::exists( tmpToolFullPath ) )
-			{
-				toolFullPath = tmpToolFullPath;
-				break;
-			}
-		}
-
-		if( toolFullPath.empty() )
-		{
-			TUTTLE_CERR(
-				"Sam command \"" + sam_tool + "\" not found."
-				"\n"
-				);
-//			displayAvailableCommands();
-			exit( -1 );
-		}
+		bfs::path toolFullPath = retrieveToolFullPath( sam_tool, searchPaths );
 		
 		const std::string fullcmd( toolFullPath.string() + " " + boost::algorithm::join( sam_cmd, " " ) );
 		
-		TUTTLE_TCOUT_VAR( fullcmd );
-		
+//		TUTTLE_TCOUT_VAR( fullcmd );
 		return system( fullcmd.c_str() );
 	}
 	catch( const boost::program_options::error& e )
