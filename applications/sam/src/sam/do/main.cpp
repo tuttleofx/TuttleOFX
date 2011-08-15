1119f207f6350c1c9e6d2ff304c25ff7ac99b1a4
Fabien Castan <fabien.castan@free.fr>
2011-08-15 16:00:11 +0200

    sam-do: split code into functions
    and check env variables: SAMDO_NODE_OPTIONS and SAMDO_unique_node_id_OPTIONS (eg. SAMDO_tuttle_blur_OPTIONS)

--- applications/sam/src/sam/do/main.cpp
+++ applications/sam/src/sam/do/main.cpp
@@ -15,6 +15,71 @@
 
 static const std::string kpipe = SAMDO_PIPE_STR;
 
+/**
+ * @brief Decomposes command line arguments into a list of options and a list of node command lines. Groups the arguments without insterpretation at this step.
+ * 
+ * @param[in] argc number of arguments ont the command line
+ * @param[in] argv list of string arguments
+ * @param[out] cl_options list of options for sam-do
+ * @param[out] cl_commands list of node command lines (list of strings groups without insterpretation at this step)
+ */
+void decomposeCommandLine( const int argc, char** const argv, std::vector<std::string>& cl_options, std::vector< std::vector<std::string> >& cl_commands )
+{
+	cl_commands.reserve(10);
+	cl_commands.resize(1); // first node for options
+
+	// split the command line to identify the multiple parts
+	for( int i = 1; i < argc; ++i )
+	{
+		std::string s( argv[i] );
+		if( s == kpipe )
+		{
+			cl_commands.resize( cl_commands.size()+1 );
+			cl_commands.back().reserve(10);
+		}
+		else
+		{
+			cl_commands.back().push_back( s );
+		}
+	}
+
+	// reoganize nodes
+	// first and last nodes may only be flags to sam-do itself
+	{
+		// First node
+		std::vector<std::string>& firstNode = cl_commands.front();
+		std::vector<std::string>::iterator fNodeIt = firstNode.begin();
+		std::vector<std::string>::iterator fNodeItEnd = firstNode.end();
+		while( fNodeIt != fNodeItEnd &&
+			   (*fNodeIt)[0] == '-' )
+		{
+			++fNodeIt;
+		}
+		// options in front of the first node are options for sam-do
+		if( firstNode.begin() != fNodeIt )
+		{
+			std::copy( firstNode.begin(), fNodeIt, std::back_inserter(cl_options) );
+			firstNode.erase( firstNode.begin(), fNodeIt );
+		}
+		if( firstNode.size() == 0 )
+		{
+			cl_commands.erase( cl_commands.begin() );
+		}
+	}
+	if( cl_commands.size() )
+	{
+		// Last node
+		// Is it a node or only flags?
+		const std::vector<std::string>& notNode = cl_commands.back();
+		if( notNode[0][0] == '-' )
+		{
+			std::copy( notNode.begin(), notNode.end(), std::back_inserter(cl_options) );
+			cl_commands.erase( cl_commands.end()-1 );
+		}
+	}
+}
+
+
 struct NodeCommand
 {
 	NodeCommand( const std::vector<std::string>& commandLine )
@@ -43,56 +108,8 @@ int main( int argc, char** argv )
 		
 		std::vector<std::string> cl_options;
 		std::vector< std::vector<std::string> > cl_commands;
-		cl_commands.reserve(10);
-		cl_commands.resize(1); // first node for options
-
-		// split the command line to identify the multiple parts
-		for( int i = 1; i < argc; ++i )
-		{
-			std::string s( argv[i] );
-			if( s == kpipe )
-			{
-				cl_commands.resize( cl_commands.size()+1 );
-				cl_commands.back().reserve(10);
-				continue;
-			}
-			cl_commands.back().push_back( s );
-		}
-
-		// reoganize nodes
-		// first and last nodes may only be flags to sam-do itself
-		{
-			// First node
-			std::vector<std::string>& firstNode = cl_commands.front();
-			std::vector<std::string>::iterator fNodeIt = firstNode.begin();
-			std::vector<std::string>::iterator fNodeItEnd = firstNode.end();
-			while( fNodeIt != fNodeItEnd &&
-				   (*fNodeIt)[0] == '-' )
-			{
-				++fNodeIt;
-			}
-			// options in front of the first node are options for sam-do
-			if( firstNode.begin() != fNodeIt )
-			{
-				std::copy( firstNode.begin(), fNodeIt, std::back_inserter(cl_options) );
-				firstNode.erase( firstNode.begin(), fNodeIt );
-			}
-			if( firstNode.size() == 0 )
-			{
-				cl_commands.erase( cl_commands.begin() );
-			}
-		}
-		if( cl_commands.size() )
-		{
-			// Last node
-			// Is it a node or only flags?
-			const std::vector<std::string>& notNode = cl_commands.back();
-			if( notNode[0][0] == '-' )
-			{
-				std::copy( notNode.begin(), notNode.end(), std::back_inserter(cl_options) );
-				cl_commands.erase( cl_commands.end()-1 );
-			}
-		}
+		
+		decomposeCommandLine( argc, argv, cl_options, cl_commands );
 
 		namespace ttl = tuttle::host;
 
@@ -267,12 +284,6 @@ int main( int argc, char** argv )
 					
 					try
 					{
-						// parse the command line, and put the result in node_vm
-						bpo::variables_map node_vm;
-						bpo::store( bpo::command_line_parser(nodeArgs).options(all_options).positional(param_values).run(), node_vm );
-
-						bpo::notify( node_vm );
-
 						std::vector< std::string > detectedPlugins;
 						for( std::size_t i = 0; i < allNodes.size(); ++i )
 						{
@@ -310,6 +321,32 @@ int main( int argc, char** argv )
 						}
 						nodeFullName = detectedPlugins.front();
 
+						// parse the command line, and put the result in node_vm
+						bpo::variables_map node_vm;
+						bpo::store( bpo::command_line_parser(nodeArgs).options(all_options).positional(param_values).run(), node_vm );
+						if( const char* env_ptr = std::getenv("SAM_DO_NODE_OPTIONS") )
+						{
+							std::vector<std::string> envOptions;
+							std::string env( env_ptr );
+							envOptions.push_back( env );
+							bpo::store( bpo::command_line_parser(envOptions).options(all_options).run(), node_vm );
+						}
+						{
+							std::string envVarName;
+							envVarName += "SAM_DO_";
+							envVarName += boost::algorithm::replace_all_copy( nodeFullName, ".", "_" );
+							envVarName += "_OPTIONS";
+							TUTTLE_TCOUT_VAR( envVarName );
+							if( const char* env_ptr = std::getenv(envVarName.c_str()) )
+							{
+								std::vector<std::string> envOptions;
+								std::string env( env_ptr );
+								envOptions.push_back( env );
+								bpo::store( bpo::command_line_parser(envOptions).options(all_options).run(), node_vm );
+							}
+						}
+
+						bpo::notify( node_vm );
 						//TUTTLE_COUT( "[" << nodeFullName << "]" );
 
 						ttl::Graph::Node& currentNode = graph.createNode( nodeFullName );
@@ -498,7 +535,7 @@ int main( int argc, char** argv )
 //								TUTTLE_COUT( "* " << p );
 //								TUTTLE_COUT( "3: " << paramName << " => " << paramValue );
 
-								/// @todo setup the node with parameter value in tuttle.
+								// setup the node with parameter value in tuttle.
 								if( paramName.size() )
 								{
 									currentNode.getParam( paramName ).setValueFromExpression( paramValue );
