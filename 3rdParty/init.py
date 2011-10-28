fcf1c38d9f477dac020bf7d64b1a4e92854676da
Marc-Antoine ARNAUD <mrn@mikrosimage.eu>
2011-10-28 11:09:53 +0200

    modification to use script with python3 and 2.6

--- 3rdParty/init.py
+++ 3rdParty/init.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import errno
 import sys
 import urllib
 import subprocess
@@ -20,30 +21,11 @@ global current_file # global variable used in dlProgress function
 current_file = ''
 download_dir = "archive"
 
-
-def movetree(src, dst, symlinks=False, ignore=None):
+def movetree(src, dst, symlinks=False):
 	names = os.listdir(src)
-	if ignore is not None:
-		ignored_names = ignore(src, names)
-	else:
-		ignored_names = set()
-
-	if not os.path.exists(dst):
-		try:
-			os.makedirs(dst)
-		except OSError, exc:
-			# XXX - this is pretty ugly
-			if "file already exists" in exc[1]:  # Windows
-				pass
-			elif "File exists" in exc[1]:        # Linux
-				pass
-			else:
-				raise
-
+	os.makedirs(dst)
 	errors = []
 	for name in names:
-		if name in ignored_names:
-			continue
 		srcname = os.path.join(src, name)
 		dstname = os.path.join(dst, name)
 		try:
@@ -51,26 +33,27 @@ def movetree(src, dst, symlinks=False, ignore=None):
 				linkto = os.readlink(srcname)
 				os.symlink(linkto, dstname)
 			elif os.path.isdir(srcname):
-				movetree(srcname, dstname, symlinks, ignore)
+				movetree(srcname, dstname, symlinks)
+				#copytree(srcname, dstname, symlinks)
 			else:
 				move(srcname, dstname)
 				#copy2(srcname, dstname)
-				# XXX What about devices, sockets etc.?
-		except (IOError, os.error), why:
+			# XXX What about devices, sockets etc.?
+		except (IOError, os.error) as why:
 			errors.append((srcname, dstname, str(why)))
-			# catch the Error from the recursive movetree so that we can
-			# continue with other files
-		except Error, err:
+		# catch the Error from the recursive copytree so that we can
+		# continue with other files
+		except Error as err:
 			errors.extend(err.args[0])
-		try:
-			copystat(src, dst)
-		except WindowsError:
-			# can't copy file access times on Windows
-			pass
-		except OSError, why:
-			errors.extend((src, dst, str(why)))
-		if errors:
-			raise Error, errors
+	try:
+		copystat(src, dst)
+	except WindowsError:
+		# can't copy file access times on Windows
+		pass
+	except OSError as why:
+		errors.extend((src, dst, str(why)))
+	if errors:
+		raise Error(errors)
 
 def dlProgress(count, blockSize, totalSize):
 	pcent = int( count * blockSize * 100/totalSize )
@@ -120,7 +103,7 @@ def uncompress(filename, ext, inNewDirectory, libname, folderExtracted):
 	if not os.path.exists( os.path.join( os.getcwd(), libname)):
 		os.mkdir(os.path.join( os.getcwd() ,libname))
 	
-	print '\nuncompress : ', folderExtracted, ' -> ', libname, '\n'
+	print ('\nuncompress : %s -> %s\n' % ( folderExtracted, libname ) )
 	
 	if os.path.exists( libname + '/Jamfile.v2' ) :
 		movetree( libname , folderExtracted )
@@ -128,12 +111,12 @@ def uncompress(filename, ext, inNewDirectory, libname, folderExtracted):
 		rmtree( libname )
 	os.rename( folderExtracted, libname )
 
-	print 'end of uncompress\n'
+	print ('end of uncompress\n')
 
 def getAndUncompress( libraries ):
 	for libname, url, folderExtracted, inNewDirectory in libraries:
-		print '_'*80
-		print '--', libname
+		print ('%s' % ('_'*80))
+		print ('-- %s'% libname)
 		parts = url.split('/')
 		filename = [p for p in parts if len(getKnownExtensions(p))]
 		#if len(filename) == 0:
@@ -142,24 +125,24 @@ def getAndUncompress( libraries ):
 		#	print '-'*40
 		#	continue
 		filename = filename[0]
-		print url, ' -> ', filename
+		print ( '%s -> %s' % (url, filename) )
 		ext = getKnownExtensions(filename)[0]
 		current_file = filename
 		try:
 			if not os.path.exists( download_dir ):
 				os.makedirs( download_dir )
 			if os.path.isfile( download_dir + "/" + filename ): # if not already downloaded
-				print 'Already downloaded: ', filename
+				print ('Already downloaded: %s' % filename )
 			else:
 				urllib.urlretrieve(url, download_dir + "/" + filename, dlProgress)
 			dirname = filename[:-len(ext)-1]
 			#if os.path.isdir(libname) or os.path.islink(libname): # if not already uncompressed
 			if os.path.isdir(filename[:-len(ext)-1]) :
-				print 'Already uncompressed : ', dirname
+				print ('Already uncompressed : %s' % dirname )
 			else:
 				uncompress( filename, ext, inNewDirectory, libname, folderExtracted )
-		except Exception, e:
-			print 'uncompress error (', str(e), ')'
+		except Exception as e:
+			print ( 'uncompress error (%s)' % str(e) )
 
 allLibs = [
 		('zlib',        'http://prdownloads.sourceforge.net/libpng/zlib-1.2.3.tar.gz'                           , 'zlib-1.2.3'                 , False) if not windows else ('zlib'    , 'http://downloads.sourceforge.net/project/libpng/zlib/1.2.3/zlib123.zip'                      , '', True),
@@ -205,19 +188,19 @@ def insertInFile( filename, linesIndexes, textToAppend ):
 	os.rename('tmp.txt', filename)
 
 def makeModificationIfNecessaryInFile( filename, headerToInsert, atLine ):
-	print filename
+	print ( '%s' % filename )
 	modification = 0
 	if(os.path.exists( filename )):
 		for line in open(filename,'r'):
 			if headerToInsert in line:
-	   			modification = 1
-	   			break
-	   	if modification == 0 :
-	   		insertInFile(filename, (1, atLine) , ("", headerToInsert ) )
-	   	else:
-	   		print "header is already include."
+				modification = 1
+				break
+		if modification == 0 :
+			insertInFile(filename, (1, atLine) , ("", headerToInsert ) )
+		else:
+			print ( 'header is already include.' )
 	else:
-		print "file is not present."
+		print ( 'file is not present.' )
 
 if len( sys.argv ) == 1:
 	getAndUncompress( allLibs )
@@ -229,8 +212,8 @@ else:
 	for libname in sys.argv[1:]:
 		getAndUncompress( [libDic[libname]] )
 
-print '_'*80
-print '-'*29 + ' Files modifications  ' + '-'*29
+print ( '%s' % ('_'*80) )
+print ( '%s Files modifications  %s' % ( ('-'*29) , ('-'*29) ) )
 
 filename = "ctl/IlmCtl/CtlLex.cpp" 
 headerToInsert = "#include <cstdlib>"
@@ -242,5 +225,5 @@ headerToInsert = "#include <cstring>"
 atLine = 55
 makeModificationIfNecessaryInFile(filename, headerToInsert, atLine );
 
-print 'End of initialisation.'
+print ( 'End of initialisation.' )
 
