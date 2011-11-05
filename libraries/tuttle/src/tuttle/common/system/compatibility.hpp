a630ee4fc84f1396c021aca315aba9cf90d4c75e
Fabien Castan <fabien.castan@free.fr>
2011-11-05 20:28:06 +0100

    tuttle common: windows compilation fix

--- libraries/tuttle/src/tuttle/common/system/compatibility.hpp
+++ libraries/tuttle/src/tuttle/common/system/compatibility.hpp
@@ -1,15 +1,20 @@
 #ifndef _TUTTLE_COMMON_SYSTEM_COMPATIBILITY_HPP_
 #define _TUTTLE_COMMON_SYSTEM_COMPATIBILITY_HPP_
 
-//#include <cstddef>
+#include <cstddef>
+
+#ifdef _MSC_VER
+#include <BaseTsd.h>
+#else
 #include <unistd.h>
+#endif
 
 // compatibility problems...
 namespace std {
 #ifdef _MSC_VER
-       typedef SSIZE_T ssize_t;
+	typedef SSIZE_T ssize_t;
 #else
-       typedef ::ssize_t ssize_t;
+	typedef ::ssize_t ssize_t;
 #endif
 }
 
