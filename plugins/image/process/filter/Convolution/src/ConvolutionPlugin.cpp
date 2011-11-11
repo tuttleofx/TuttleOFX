de528d3d02c1b72f95447ecf5f4f9e3921b4a6f8
Fabien Castan <fabien.castan@free.fr>
2011-11-11 23:40:24 +0100

    Convolution: first version, only implement separable filters

--- plugins/image/process/filter/Convolution/src/ConvolutionPlugin.cpp
+++ plugins/image/process/filter/Convolution/src/ConvolutionPlugin.cpp
@@ -14,6 +14,7 @@ ConvolutionPlugin::ConvolutionPlugin( OfxImageEffectHandle handle )
 	: ImageEffectGilPlugin( handle )
 {
 	_paramSize = fetchInt2DParam( kParamSize );
+	_paramBorder = fetchChoiceParam( kParamBorder );
 
 	_paramCoef.resize( kParamSizeMax );
 	for( unsigned int y = 0; y < kParamSizeMax; ++y )
@@ -34,7 +35,35 @@ ConvolutionProcessParams ConvolutionPlugin::getProcessParams() const
 
 	params._size.x = boost::numeric_cast<unsigned int>( size.x );
 	params._size.y = boost::numeric_cast<unsigned int>( size.y );
-
+	
+	params._boundary_option = terry::convolve_option_extend_mirror;
+	params._border = static_cast<EParamBorder>( _paramBorder->getValue() );
+	switch( params._border )
+	{
+		case eParamBorderMirror:
+			params._boundary_option = terry::convolve_option_extend_mirror;
+			break;
+		case eParamBorderConstant:
+			params._boundary_option = terry::convolve_option_extend_constant;
+			break;
+		case eParamBorderBlack:
+			params._boundary_option = terry::convolve_option_extend_zero;
+			break;
+		case eParamBorderPadded:
+			params._boundary_option = terry::convolve_option_extend_padded;
+			break;
+	}
+	params._convX.resize( params._size.x );
+	params._convY.resize( params._size.y );
+	for( unsigned int i = 0; i < params._size.x; ++i )
+	{
+		params._convX[i] = _paramCoef[0][i]->getValue();
+	}
+	for( unsigned int i = 0; i < params._size.y; ++i )
+	{
+		params._convY[i] = _paramCoef[1][i]->getValue();
+	}
+	/*
 	params._convMatrix.resize( params._size.x, params._size.y );
 	for( unsigned int y = 0; y < params._size.y; ++y )
 	{
@@ -45,6 +74,7 @@ ConvolutionProcessParams ConvolutionPlugin::getProcessParams() const
 			TUTTLE_COUT( "coef[" << y << "][" << x << "] = " << params._convMatrix(x, y) );
 		}
 	}
+	*/
 	return params;
 }
 
@@ -57,10 +87,29 @@ void ConvolutionPlugin::render( const OFX::RenderArguments& args )
 	doGilRender<ConvolutionProcess>( *this, args );
 }
 
+bool ConvolutionPlugin::getRegionOfDefinition( const OFX::RegionOfDefinitionArguments& args, OfxRectD& rod )
+{
+	const ConvolutionProcessParams params = getProcessParams();
+	const OfxRectD srcRod = _clipSrc->getCanonicalRod( args.time );
+	
+	switch( params._border )
+	{
+		case eParamBorderPadded:
+			rod.x1 = srcRod.x1 + params._convX.left_size();
+			rod.y1 = srcRod.y1 + params._convY.left_size();
+			rod.x2 = srcRod.x2 - params._convX.right_size();
+			rod.y2 = srcRod.y2 - params._convY.right_size();
+			return true;
+		default:
+			break;
+	}
+	return false;
+}
+
 void ConvolutionPlugin::getRegionsOfInterest( const OFX::RegionsOfInterestArguments& args, OFX::RegionOfInterestSetter& rois )
 {
 	OfxRectD srcRoi = args.regionOfInterest;
-	OfxPointI size  = _paramSize->getValue();
+	const OfxPointI size  = _paramSize->getValue();
 	OfxPointD halfSize;
 
 	halfSize.x = size.x * 0.5;
@@ -72,22 +121,42 @@ void ConvolutionPlugin::getRegionsOfInterest( const OFX::RegionsOfInterestArgume
 	rois.setRegionOfInterest( *_clipSrc, srcRoi );
 }
 
+bool ConvolutionPlugin::isIdentity( const OFX::RenderArguments& args, OFX::Clip*& identityClip, double& identityTime )
+{
+	const OfxPointI size  = _paramSize->getValue();
+	if( size.x != 0 || size.y != 0 )
+		return false;
+
+	identityClip = _clipSrc;
+	identityTime = args.time;
+	return true;
+}
+
 void ConvolutionPlugin::changedParam( const OFX::InstanceChangedArgs& args, const std::string& paramName )
 {
 	if( paramName == kParamSize && args.reason == OFX::eChangeUserEdit )
 	{
-		OfxPointI v = _paramSize->getValue();
+		const OfxPointI v = _paramSize->getValue();
 		OfxPointI oddNumber;
 		oddNumber.x = v.x | 1; // odd number
 		oddNumber.y = v.y | 1; // odd number
 		if( oddNumber != v )
 			_paramSize->setValue( oddNumber );
-	}
-	else if( paramName == kHelpButton )
-	{
-		sendMessage( OFX::Message::eMessageMessage,
-		             "", // No XML resources
-		             kHelpString );
+		
+		// separable convolution
+		for( unsigned int x = 0; x < kParamSizeMax; ++x )
+		{
+			_paramCoef[0][x]->setIsSecretAndDisabled( x >= v.x );
+			_paramCoef[1][x]->setIsSecretAndDisabled( x >= v.y );
+		}
+		// disable other matrix values
+		for( unsigned int y = 2; y < kParamSizeMax; ++y )
+		{
+			for( unsigned int x = 0; x < kParamSizeMax; ++x )
+			{
+				_paramCoef[y][x]->setIsSecretAndDisabled( true );
+			}
+		}
 	}
 }
 
--- plugins/image/process/filter/Convolution/src/ConvolutionPlugin.hpp
+++ plugins/image/process/filter/Convolution/src/ConvolutionPlugin.hpp
@@ -1,9 +1,14 @@
 #ifndef _TUTTLE_PLUGIN_CONVOLUTION_PLUGIN_HPP_
 #define _TUTTLE_PLUGIN_CONVOLUTION_PLUGIN_HPP_
 
+#include "ConvolutionDefinitions.hpp"
+
 #include <tuttle/plugin/ImageEffectGilPlugin.hpp>
 
-#include <boost/numeric/ublas/matrix.hpp>
+#include <terry/numeric/kernel.hpp>
+#include <terry/numeric/convolve.hpp>
+
+//#include <boost/numeric/ublas/matrix.hpp>
 
 #include <vector>
 
@@ -13,8 +18,17 @@ namespace convolution {
 
 struct ConvolutionProcessParams
 {
+//	boost::numeric::ublas::matrix<double> _convMatrix;
+	typedef float Scalar;
+	typedef typename terry::kernel_1d<Scalar> Kernel;
+	
 	boost::gil::point2<unsigned int> _size;
-	boost::numeric::ublas::matrix<double> _convMatrix;
+	
+	EParamBorder _border;
+	terry::convolve_boundary_option _boundary_option;
+	
+	Kernel _convX;
+	Kernel _convY;
 };
 
 /**
@@ -27,13 +41,18 @@ public:
 
 public:
 	void render( const OFX::RenderArguments& args );
+	
+	bool isIdentity( const OFX::RenderArguments& args, OFX::Clip*& identityClip, double& identityTime );
 	void getRegionsOfInterest( const OFX::RegionsOfInterestArguments& args, OFX::RegionOfInterestSetter& rois );
+	bool getRegionOfDefinition( const OFX::RegionOfDefinitionArguments& args, OfxRectD& rod );
+
 	void changedParam( const OFX::InstanceChangedArgs& args, const std::string& paramName );
 
 	ConvolutionProcessParams getProcessParams() const;
 
 public:
 	OFX::Int2DParam* _paramSize;
+	OFX::ChoiceParam* _paramBorder;
 	std::vector<std::vector<OFX::DoubleParam*> > _paramCoef;
 };
 
