1dd9e8283757af62725d2a5f73643ed50d621c65
Marc-Antoine ARNAUD <mrn@mikrosimage.eu>
2011-12-12 17:39:55 +0100

    change parameters name for command line compatibility

--- plugins/image/process/color/Colorspace/src/ColorSpaceDefinitions.hpp
+++ plugins/image/process/color/Colorspace/src/ColorSpaceDefinitions.hpp
@@ -11,51 +11,51 @@ namespace colorspace {
 static const bool kSupportTiles				= true;
 static const bool kSupportTemporalClipAccess		= true;
 
-static const std::string kColorSpaceIn			= "In";
-static const std::string kColorSpaceOut			= "Out";
+static const std::string kColorSpaceIn			= "in";
+static const std::string kColorSpaceOut			= "out";
 
-static const std::string kColorSpaceReferenceSpaceIn	= "Input Reference Space";
-static const std::string kColorSpaceReferenceSpaceOut	= "Output Reference Space";
+static const std::string kColorSpaceReferenceSpaceIn	= "inputReferenceSpace";
+static const std::string kColorSpaceReferenceSpaceOut	= "outputReferenceSpace";
 
-static const std::string kColorSpaceCustomizedIn	= "Input Customized";
-static const std::string kColorSpaceCustomizedOut	= "Output Customized";
+static const std::string kColorSpaceCustomizedIn	= "inputCustomized";
+static const std::string kColorSpaceCustomizedOut	= "outputCustomized";
 
-static const std::string kColorSpaceGradationLawIn	= "In Gradation Law";
-static const std::string kColorSpaceGradationLawOut	= "Out Gradation Law";
+static const std::string kColorSpaceGradationLawIn	= "inGradationLaw";
+static const std::string kColorSpaceGradationLawOut	= "outGradationLaw";
 
 
-static const std::string kColorSpaceInGammaValue	= "In Gamma Value";
-static const std::string kColorSpaceInBlackPoint	= "In Black Point";
-static const std::string kColorSpaceInWhitePoint	= "In White Point";
-static const std::string kColorSpaceInGammaSensito	= "In Gamma Sensito";
+static const std::string kColorSpaceInGammaValue	= "inGammaValue";
+static const std::string kColorSpaceInBlackPoint	= "inBlackPoint";
+static const std::string kColorSpaceInWhitePoint	= "inWhitePoint";
+static const std::string kColorSpaceInGammaSensito	= "inGammaSensito";
 
-static const std::string kColorSpaceOutGammaValue	= "Out Gamma Value";
-static const std::string kColorSpaceOutBlackPoint	= "Out Black Point";
-static const std::string kColorSpaceOutWhitePoint	= "Out White Point";
-static const std::string kColorSpaceOutGammaSensito	= "Out Gamma Sensito";
+static const std::string kColorSpaceOutGammaValue	= "outGammaValue";
+static const std::string kColorSpaceOutBlackPoint	= "outBlackPoint";
+static const std::string kColorSpaceOutWhitePoint	= "outWhitePoint";
+static const std::string kColorSpaceOutGammaSensito	= "outGammaSensito";
 
-static const std::string kColorSpaceLayoutIn		= "In Layout";
-static const std::string kColorSpaceLayoutOut		= "Out Layout";
+static const std::string kColorSpaceLayoutIn		= "inLayout";
+static const std::string kColorSpaceLayoutOut		= "outLayout";
 
-static const std::string kColorSpaceTempColorIn		= "In Color Temperature";
-static const std::string kColorSpaceTempColorOut	= "Out Color Temperature";
+static const std::string kColorSpaceTempColorIn		= "inColorTemperature";
+static const std::string kColorSpaceTempColorOut	= "outColorTemperature";
 
-static const std::string kColorSpacePrimariesIn		= "In Primaries";
-static const std::string kColorSpacePrimariesOut	= "Out Primaries";
+static const std::string kColorSpacePrimariesIn		= "inPrimaries";
+static const std::string kColorSpacePrimariesOut	= "outPrimaries";
 
-static const std::string kColorSpaceXrIn		= "In Xr";
-static const std::string kColorSpaceYrIn		= "In Yr";
-static const std::string kColorSpaceXgIn		= "In Xg";
-static const std::string kColorSpaceYgIn		= "In Yg";
-static const std::string kColorSpaceXbIn		= "In Xb";
-static const std::string kColorSpaceYbIn		= "In Yb";
+static const std::string kColorSpaceXrIn		= "inXr";
+static const std::string kColorSpaceYrIn		= "inYr";
+static const std::string kColorSpaceXgIn		= "inXg";
+static const std::string kColorSpaceYgIn		= "inYg";
+static const std::string kColorSpaceXbIn		= "inXb";
+static const std::string kColorSpaceYbIn		= "inYb";
 
-static const std::string kColorSpaceXrOut		= "Out Xr";
-static const std::string kColorSpaceYrOut		= "Out Yr";
-static const std::string kColorSpaceXgOut		= "Out Xg";
-static const std::string kColorSpaceYgOut		= "Out Yg";
-static const std::string kColorSpaceXbOut		= "Out Xb";
-static const std::string kColorSpaceYbOut		= "Out Yb";
+static const std::string kColorSpaceXrOut		= "outXr";
+static const std::string kColorSpaceYrOut		= "outYr";
+static const std::string kColorSpaceXgOut		= "outXg";
+static const std::string kColorSpaceYgOut		= "outYg";
+static const std::string kColorSpaceXbOut		= "outXb";
+static const std::string kColorSpaceYbOut		= "outYb";
 
 }
 }
