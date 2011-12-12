c67ea1be84024c1c3350015ec6f9df41a1f6ca4f
Marc-Antoine ARNAUD <mrn@mikrosimage.eu>
2011-12-12 17:36:32 +0100

    remove help button, add help, and start to add compression options for EXR

--- plugins/image/io/OpenImageIO/src/writer/OpenImageIOWriterDefinitions.hpp
+++ plugins/image/io/OpenImageIO/src/writer/OpenImageIOWriterDefinitions.hpp
@@ -25,6 +25,32 @@ enum EParamComponents
 	eParamComponentsRGBA = 0,
 	eParamComponentsRGB
 };
+
+static const std::string kParamOutputCompression  = "compression";
+
+static const std::string kParamOutputCompressionNone  = "none   No compression";
+static const std::string kParamOutputCompressionZip   = "zip    Compression zlib, with blocs of 16 scanlines.";
+static const std::string kParamOutputCompressionZips  = "zips   Compression zlib, with blocs with only one scanline.";
+static const std::string kParamOutputCompressionRle   = "rle    Compression Run Length Encoding.";
+static const std::string kParamOutputCompressionPiz   = "piz    Piz-based wavelet compression.";
+static const std::string kParamOutputCompressionPxr24 = "pxr24  Compression (with loss) in 24bit float";
+static const std::string kParamOutputCompressionB44   = "b44    Compression (with loss) with blocs of size 4x4 pixels. Fix rate of compression.";
+static const std::string kParamOutputCompressionB44a  = "b44a   Compression (with loss) with blocs of size 4x4 pixels. Non-fix rate of compression.";
+
+
+enum EParamCompression
+{
+	eParamCompressionNone = 0,
+	eParamCompressionZip,
+	eParamCompressionZips,
+	eParamCompressionRle,
+	eParamCompressionPiz,
+	eParamCompressionPxr24,
+	eParamCompressionB44,
+	eParamCompressionB44a
+};
+
+
 }
 }
 }
