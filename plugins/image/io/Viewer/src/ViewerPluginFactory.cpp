7ecf43c9e20856d3cb7bbe2f530ff2eb426f082f
Marc-Antoine ARNAUD <mrn@mikrosimage.eu>
2011-12-15 18:04:26 +0100

    remove Byte and Short support in viewer, show images only in Float

--- plugins/image/io/Viewer/src/ViewerPluginFactory.cpp
+++ plugins/image/io/Viewer/src/ViewerPluginFactory.cpp
@@ -28,8 +28,8 @@ void ViewerPluginFactory::describe( OFX::ImageEffectDescriptor& desc )
 	desc.addSupportedContext( OFX::eContextGeneral );
 
 	// add supported pixel depths
-	desc.addSupportedBitDepth( OFX::eBitDepthUByte );
-	desc.addSupportedBitDepth( OFX::eBitDepthUShort );
+	//desc.addSupportedBitDepth( OFX::eBitDepthUByte );
+	//desc.addSupportedBitDepth( OFX::eBitDepthUShort );
 	desc.addSupportedBitDepth( OFX::eBitDepthFloat );
 
 	// plugin flags
