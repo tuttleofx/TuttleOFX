0db5facb6707300a2e812dfe5753b1d696db44a3
Marc-Antoine ARNAUD <mrn@mikrosimage.eu>
2012-01-03 11:39:05 +0100

    change color managment, add explicit color definition (Red, Yellow, Blue, Green, Cyan, Magenta, Black, White)

--- libraries/tuttle/src/tuttle/common/utils/color.hpp
+++ libraries/tuttle/src/tuttle/common/utils/color.hpp
@@ -7,17 +7,36 @@ namespace tuttle {
 namespace common {
 
 #ifdef __LINUX__
-static const std::string kColorStd      ( "\E[0;0m"  );
-static const std::string kColorFolder   ( "\E[1;34m" );
-static const std::string kColorFile     ( "\E[0;32m" );
-static const std::string kColorSequence ( "\E[0;32m" );
-static const std::string kColorError    ( "\E[1;31m" );
+static const std::string kColorBlack    ( "\E[1;30m" );
+static const std::string kColorWhite    ( "\E[1;37m" );
+static const std::string kColorBlue     ( "\E[1;34m" );
+static const std::string kColorGreen    ( "\E[1;32m" );
+static const std::string kColorRed      ( "\E[1;31m" );
+static const std::string kColorCyan     ( "\E[1;36m" );
+static const std::string kColorMagenta  ( "\E[1;35m" );
+static const std::string kColorYellow   ( "\E[1;33m" );
+
+static const std::string kColorStd      ( "\E[0;0m"   );
+static const std::string kColorFolder   ( kColorBlue  );
+static const std::string kColorFile     ( kColorGreen );
+static const std::string kColorSequence ( kColorGreen );
+static const std::string kColorError    ( kColorRed   );
 #else
+static const std::string kColorBlack    ( "" );
+static const std::string kColorWhite    ( "" );
+static const std::string kColorBlue     ( "" );
+static const std::string kColorGreen    ( "" );
+static const std::string kColorRed      ( "" );
+static const std::string kColorCyan     ( "" );
+static const std::string kColorMagenta  ( "" );
+static const std::string kColorYellow   ( "" );
+
 static const std::string kColorStd      ( "" );
 static const std::string kColorFolder   ( "" );
 static const std::string kColorFile     ( "" );
 static const std::string kColorSequence ( "" );
 static const std::string kColorError    ( "" );
+static const std::string kColorYellow   ( "" );
 #endif
 
 }
