0bcf092e82494d200a11e00e3988ba90ab892e85
Marc-Antoine ARNAUD <mrn@mikrosimage.eu>
2012-01-19 16:11:43 +0100

    create directory if it not exist, not throx an exception

--- libraries/tuttle/src/tuttle/plugin/context/WriterPlugin.cpp
+++ libraries/tuttle/src/tuttle/plugin/context/WriterPlugin.cpp
@@ -8,19 +8,19 @@ namespace plugin {
 namespace bfs = boost::filesystem;
 
 WriterPlugin::WriterPlugin( OfxImageEffectHandle handle )
-	: ImageEffectGilPlugin( handle )
-	, _oneRender( false )
-	, _oneRenderAtTime( 0 )
+    : ImageEffectGilPlugin( handle )
+    , _oneRender( false )
+    , _oneRenderAtTime( 0 )
 {
-	_clipSrc             = fetchClip( kOfxImageEffectSimpleSourceClipName );
-	_clipDst             = fetchClip( kOfxImageEffectOutputClipName );
-	_paramFilepath       = fetchStringParam( kParamWriterFilename );
-	_paramRenderButton   = fetchPushButtonParam( kParamWriterRender );
-	_paramRenderAlways   = fetchBooleanParam( kParamWriterRenderAlways );
-	_paramBitDepth       = fetchChoiceParam( kParamWriterBitDepth );
-	_paramForceNewRender = fetchIntParam( kParamWriterForceNewRender );
-	_paramFlip           = fetchBooleanParam( kParamWriterFlip );
-	_isSequence          = _filePattern.initFromDetection( _paramFilepath->getValue() );
+    _clipSrc             = fetchClip( kOfxImageEffectSimpleSourceClipName );
+    _clipDst             = fetchClip( kOfxImageEffectOutputClipName );
+    _paramFilepath       = fetchStringParam( kParamWriterFilename );
+    _paramRenderButton   = fetchPushButtonParam( kParamWriterRender );
+    _paramRenderAlways   = fetchBooleanParam( kParamWriterRenderAlways );
+    _paramBitDepth       = fetchChoiceParam( kParamWriterBitDepth );
+    _paramForceNewRender = fetchIntParam( kParamWriterForceNewRender );
+    _paramFlip           = fetchBooleanParam( kParamWriterFlip );
+    _isSequence          = _filePattern.initFromDetection( _paramFilepath->getValue() );
 }
 
 WriterPlugin::~WriterPlugin()
@@ -29,57 +29,57 @@ WriterPlugin::~WriterPlugin()
 
 void WriterPlugin::changedParam( const OFX::InstanceChangedArgs& args, const std::string& paramName )
 {
-	if( paramName == kParamWriterFilename )
-	{
-		_isSequence = _filePattern.initFromDetection( _paramFilepath->getValue() );
-	}
-	else if( paramName == kParamWriterRender )
-	{
-		_oneRender       = true;
-		_oneRenderAtTime = args.time;
-		// modify the plugin to force a new render
-		_paramForceNewRender->setValue( _paramForceNewRender->getValue() + 1 );
-	}
+    if( paramName == kParamWriterFilename )
+    {
+        _isSequence = _filePattern.initFromDetection( _paramFilepath->getValue() );
+    }
+    else if( paramName == kParamWriterRender )
+    {
+        _oneRender       = true;
+        _oneRenderAtTime = args.time;
+        // modify the plugin to force a new render
+        _paramForceNewRender->setValue( _paramForceNewRender->getValue() + 1 );
+    }
 }
 
 void WriterPlugin::getClipPreferences( OFX::ClipPreferencesSetter& clipPreferences )
 {
-	// If pattern detected (frame varying on time)
-	clipPreferences.setOutputFrameVarying( varyOnTime() );
+    // If pattern detected (frame varying on time)
+    clipPreferences.setOutputFrameVarying( varyOnTime() );
 }
 
 bool WriterPlugin::isIdentity( const OFX::RenderArguments& args, OFX::Clip*& identityClip, OfxTime& identityTime )
 {
-	// little hack for the push button Render
-	if( _oneRender && _oneRenderAtTime == args.time )
-	{
-		return false;
-	}
-	if( OFX::getImageEffectHostDescription()->hostIsBackground )
-	{
-		return false;
-	}
-	if( _paramRenderAlways->getValue() )
-	{
-		return false;
-	}
-	identityClip = _clipSrc;
-	identityTime = args.time;
-	return true;
+    // little hack for the push button Render
+    if( _oneRender && _oneRenderAtTime == args.time )
+    {
+        return false;
+    }
+    if( OFX::getImageEffectHostDescription()->hostIsBackground )
+    {
+        return false;
+    }
+    if( _paramRenderAlways->getValue() )
+    {
+        return false;
+    }
+    identityClip = _clipSrc;
+    identityTime = args.time;
+    return true;
 }
 
 void WriterPlugin::beginSequenceRender( const OFX::BeginSequenceRenderArguments& args )
 {
-	boost::filesystem::path dir( getAbsoluteDirectory() );
-	if( ! boost::filesystem::exists( dir ) )
-	{
-		BOOST_THROW_EXCEPTION( exception::NoDirectory( dir.string() ) );
-	}
+    boost::filesystem::path dir( getAbsoluteDirectory() );
+    if( ! boost::filesystem::exists( dir ) )
+    {
+        boost::filesystem::create_directories( dir );
+    }
 }
 
 void WriterPlugin::render( const OFX::RenderArguments& args )
 {
-	_oneRender = false;
+    _oneRender = false;
 }
 
 }
