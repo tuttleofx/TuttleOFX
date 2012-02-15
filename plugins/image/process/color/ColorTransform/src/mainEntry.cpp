74ac747a41575f4acd5a68734ba6eb7ede9ca4c2
Marc-Antoine ARNAUD <mrn@mikrosimage.eu>
2012-02-15 17:13:52 +0100

    change node name

--- plugins/image/process/color/ColorTransform/src/mainEntry.cpp
+++ plugins/image/process/color/ColorTransform/src/mainEntry.cpp
@@ -9,7 +9,7 @@ namespace Plugin {
 
 void getPluginIDs( OFX::PluginFactoryArray& ids )
 {
-	mAppendPluginFactory( ids, tuttle::plugin::colorTransform::ColorTransformPluginFactory, ".tuttle.colortransform" );
+	mAppendPluginFactory( ids, tuttle::plugin::colorTransform::ColorTransformPluginFactory, "tuttle.colortransform" );
 }
 
 }
