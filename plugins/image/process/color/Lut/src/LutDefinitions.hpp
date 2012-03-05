8fd13e248bb8d8cfd59b873282089442d559ef6d
Marc-Antoine ARNAUD <mrn@mikrosimage.eu>
2012-03-05 18:38:21 +0100

    clean with common Filename parameter

--- plugins/image/process/color/Lut/src/LutDefinitions.hpp
+++ plugins/image/process/color/Lut/src/LutDefinitions.hpp
@@ -1,12 +1,13 @@
 #ifndef _TUTTLE_PLUGIN_LUTDEFINITIONS_HPP_
 #define _TUTTLE_PLUGIN_LUTDEFINITIONS_HPP_
 
+#include <tuttle/plugin/context/Definition.hpp>
+
 namespace tuttle {
 namespace plugin {
 namespace lut {
 
 static const std::string kHelp      = "help";
-static const std::string kInputFilename      = "filename";
 static const std::string kInputFilenameLabel = "3D Lut input filename";
 
 }
