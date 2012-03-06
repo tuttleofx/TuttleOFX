bec7e9dda06253f0fc7adb783008de3b73114d6f
Marc-Antoine ARNAUD <mrn@mikrosimage.eu>
2012-03-06 10:55:50 +0100

    update imageMagick library

--- 3rdParty/init.py
+++ 3rdParty/init.py
@@ -173,7 +173,7 @@ allLibs = [
 		('ffmpeg',      'http://ffmpeg.org/releases/ffmpeg-0.8.2.tar.bz2'                                                           , 'ffmpeg-0.8.2'                   , False),
 		('openimageio', 'https://github.com/OpenImageIO/oiio/tarball/RB-1.0/OpenImageIO-oiio-Release-1.0.0-0-g5b37f1c.tar.gz'       , 'OpenImageIO-oiio-1d4673f'       , False),
 		('opencolorio', 'http://github.com/imageworks/OpenColorIO/tarball/v1.0.4/imageworks-OpenColorIO-v1.0.4-0-gc0a9a92.tar.gz'   , 'imageworks-OpenColorIO-12fb440' , False),
-		('imagemagick', 'http://www.imagemagick.org/download/ImageMagick.tar.gz'                                                    , 'ImageMagick-6.7.5-6'            , False)
+		('imagemagick', 'http://www.imagemagick.org/download/ImageMagick.tar.gz'                                                    , 'ImageMagick-6.7.5-10'            , False)
 	]
 
 
