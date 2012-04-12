b1942ba841a2acafca7732cf41ec192281bb7870
Marc-Antoine ARNAUD <arnaud.marcnatoine@gmail.com>
2012-04-12 18:49:00 +0200

    change configure to build correctly ImageMagick

--- 3rdParty/configure.py
+++ 3rdParty/configure.py
@@ -29,7 +29,7 @@ os.system( "./configure" )
 os.chdir ( ".." )
 
 os.chdir ( "./imagemagick" )
-os.system( "./configure" )
+os.system( "./configure --disable-opencl --disable-openmp --with-gnu-ld=no --enable-hdri=no --with-bzlib=no --with-autotrace=no --with-djvu=no  --with-dps=no  --with-fftw=no  --with-fpx=no --with-fontconfig=no --with-freetype=no --with-gslib=no --with-gvc=no --with-jbig=no --with-jpeg=no --with-jp2=no --with-lcms=no --with-lcms2=no --with-lqr=no --with-lzma=no --with-magick-plus-plus=no --with-openexr=no --with-perl=no --with-pango=no --with-png=no  --with-rsvg=no  --with-tiff=no --with-webp=no --with-wmf=no --with-x=no --with-xml=no --with-zlib=no" )
 os.chdir ( ".." )
 
 os.chdir ( "./webp" )
