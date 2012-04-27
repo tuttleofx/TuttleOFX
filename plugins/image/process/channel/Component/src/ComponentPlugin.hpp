6a0403374be42f910b97cdbc0f7094c176d89786
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2012-04-27 16:35:06 +0200

    add Component plugin

---
+++ plugins/image/process/channel/Component/src/ComponentPlugin.hpp
@@ -0,0 +1,47 @@
+#ifndef _TUTTLE_PLUGIN_COMPONENT_PLUGIN_HPP_
+#define _TUTTLE_PLUGIN_COMPONENT_PLUGIN_HPP_
+
+#include "ComponentDefinitions.hpp"
+
+#include <tuttle/plugin/ImageEffectGilPlugin.hpp>
+
+namespace tuttle {
+namespace plugin {
+namespace component {
+
+/**
+ * @brief Component plugin
+ */
+class ComponentPlugin : public ImageEffectGilPlugin
+{
+public:
+	typedef float Scalar;
+public:
+    ComponentPlugin( OfxImageEffectHandle handle );
+
+public:
+
+    void changedParam( const OFX::InstanceChangedArgs &args, const std::string &paramName );
+
+//	bool getRegionOfDefinition( const OFX::RegionOfDefinitionArguments& args, OfxRectD& rod );
+//	void getRegionsOfInterest( const OFX::RegionsOfInterestArguments& args, OFX::RegionOfInterestSetter& rois );
+	bool isIdentity( const OFX::RenderArguments& args, OFX::Clip*& identityClip, double& identityTime );
+	void getClipPreferences( OFX::ClipPreferencesSetter& clipPreferences );
+	/*
+	template< typename dstComponents >
+	void renderWithDstComponent( const OFX::RenderArguments &args, OFX::EPixelComponent srcComponents, OFX::EBitDepth bitDepth );
+	*/
+	void render( const OFX::RenderArguments &args );
+	
+public:
+	OFX::ChoiceParam*  _paramToComponent;
+	OFX::ChoiceParam*  _paramToGrayMethod;
+	OFX::BooleanParam* _paramPremultiplied;
+	
+};
+
+}
+}
+}
+
+#endif
---
+++ plugins/image/process/channel/Component/src/ComponentPluginFactory.cpp
@@ -0,0 +1,99 @@
+#include "ComponentPluginFactory.hpp"
+#include "ComponentPlugin.hpp"
+#include "ComponentDefinitions.hpp"
+#include "ofxsImageEffect.h"
+
+#include <limits>
+
+namespace tuttle {
+namespace plugin {
+namespace component {
+
+static const bool kSupportTiles = false;
+
+/**
+ * @brief Function called to describe the plugin main features.
+ * @param[in, out] desc Effect descriptor
+ */
+void ComponentPluginFactory::describe( OFX::ImageEffectDescriptor& desc )
+{
+	desc.setLabels( "Component", "Component",
+		            "Component" );
+	desc.setPluginGrouping( "tuttle" );
+
+	desc.setDescription( "Convert channels components." );
+
+	// add the supported contexts, only filter at the moment
+	desc.addSupportedContext( OFX::eContextFilter );
+	desc.addSupportedContext( OFX::eContextGeneral );
+
+	// add supported pixel depths
+	desc.addSupportedBitDepth( OFX::eBitDepthUByte );
+	desc.addSupportedBitDepth( OFX::eBitDepthUShort );
+	desc.addSupportedBitDepth( OFX::eBitDepthFloat );
+
+	// plugin flags
+	desc.setSupportsTiles( kSupportTiles );
+	desc.setRenderThreadSafety( OFX::eRenderFullySafe );
+}
+
+/**
+ * @brief Function called to describe the plugin controls and features.
+ * @param[in, out]   desc       Effect descriptor
+ * @param[in]        context    Application context
+ */
+void ComponentPluginFactory::describeInContext( OFX::ImageEffectDescriptor& desc,
+                                                  OFX::EContext context )
+{
+	OFX::ClipDescriptor* srcClip = desc.defineClip( kOfxImageEffectSimpleSourceClipName );
+	srcClip->addSupportedComponent( OFX::ePixelComponentRGBA );
+	srcClip->addSupportedComponent( OFX::ePixelComponentRGB );
+	srcClip->addSupportedComponent( OFX::ePixelComponentAlpha );
+	srcClip->setSupportsTiles( kSupportTiles );
+
+	// Create the mandated output clip
+	OFX::ClipDescriptor* dstClip = desc.defineClip( kOfxImageEffectOutputClipName );
+	dstClip->addSupportedComponent( OFX::ePixelComponentRGBA );
+	dstClip->addSupportedComponent( OFX::ePixelComponentRGB );
+	dstClip->addSupportedComponent( OFX::ePixelComponentAlpha );
+	dstClip->setSupportsTiles( kSupportTiles );
+
+	OFX::ChoiceParamDescriptor* outTo = desc.defineChoiceParam( kParamTo );
+	outTo->setLabel( kParamToLabel );
+	outTo->appendOption( kConvertToGray );
+	outTo->appendOption( kConvertToRGB );
+	outTo->appendOption( kConvertToRGBA );
+	outTo->setDefault( eConvertToRGBA );
+	
+	OFX::ChoiceParamDescriptor* outGray = desc.defineChoiceParam( kParamToGray );
+	outGray->setLabel( kParamToGrayLabel );
+	outGray->appendOption( kConvertToGrayMean );
+	outGray->appendOption( kConvertToGrayRec601 );
+	outGray->appendOption( kConvertToGrayRec709 );
+	outGray->appendOption( kConvertToGraySelectRed );
+	outGray->appendOption( kConvertToGraySelectGreen );
+	outGray->appendOption( kConvertToGraySelectBlue );
+	outGray->appendOption( kConvertToGraySelectAlpha );
+	outGray->setDefault( 3 ); // terry::color::components::eConvertToGrayRec709
+
+	OFX::BooleanParamDescriptor* outPremult = desc.defineBooleanParam( kParamPremutliplied );
+	outPremult->setLabel( kParamPremutlipliedLabel );
+	outPremult->setDefault( false );
+}
+
+/**
+ * @brief Function called to create a plugin effect instance
+ * @param[in] handle  Effect handle
+ * @param[in] context Application context
+ * @return  plugin instance
+ */
+OFX::ImageEffect* ComponentPluginFactory::createInstance( OfxImageEffectHandle handle,
+                                                            OFX::EContext context )
+{
+	return new ComponentPlugin( handle );
+}
+
+}
+}
+}
+
