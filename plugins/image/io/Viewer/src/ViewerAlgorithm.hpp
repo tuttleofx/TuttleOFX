6de725e69d304c97cfb38893d2c867ef8d548bc3
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2012-04-27 18:51:57 +0200

    Viewer: add component view (alpha is a problem, need to switch)

--- plugins/image/io/Viewer/src/ViewerAlgorithm.hpp
+++ plugins/image/io/Viewer/src/ViewerAlgorithm.hpp
@@ -51,6 +51,10 @@ const boost::gil::rgba16_view_t*  viewRgba16;
 const boost::gil::rgba32_view_t*  viewRgba32;
 const boost::gil::rgba32f_view_t* viewRgba32f;
 
+bool showRedChannel = false;
+bool showGreenChannel = false;
+bool showBlueChannel = false;
+bool showAlphaChannel = false;
 
 void reshape(int width,int height)
 {
@@ -295,6 +299,301 @@ void loadNewTexture( const boost::gil::rgba32f_view_t* src )
 	glEnable(GL_TEXTURE_2D);
 }
 
+void reloadTexture( )
+{
+	unsigned char* dataPtr = NULL;
+	
+	GLint	internalFormat = 1;
+	size_t	width=0;
+	size_t	height=0;
+	GLenum	format = GL_RGBA;
+	GLenum	type = GL_UNSIGNED_BYTE;
+	switch( loadedTexture )
+	{
+		case 0: return;
+		case 1:
+			dataPtr = (unsigned char*)boost::gil::interleaved_view_get_raw_data( *viewGray8 );
+			internalFormat = 1;
+			width  = viewGray8->width();
+			height = viewGray8->height();
+			format = GL_LUMINANCE;
+			type   = GL_UNSIGNED_BYTE;
+			break;
+		case 2:
+			dataPtr = (unsigned char*)boost::gil::interleaved_view_get_raw_data( *viewRgb8 );
+			internalFormat = 3;
+			width  = viewRgb8->width();
+			height = viewRgb8->height();
+			format = GL_RGB;
+			type   = GL_UNSIGNED_BYTE;
+			break;
+		case 3:
+			dataPtr = (unsigned char*)boost::gil::interleaved_view_get_raw_data( *viewRgba8 );
+			internalFormat = 4;
+			width  = viewRgba8->width();
+			height = viewRgba8->height();
+			format = GL_RGBA;
+			type   = GL_UNSIGNED_BYTE;
+			break;
+		case 4:
+			dataPtr = (unsigned char*)boost::gil::interleaved_view_get_raw_data( *viewGray16 );
+			internalFormat = 1;
+			width  = viewGray16->width();
+			height = viewGray16->height();
+			format = GL_LUMINANCE;
+			type   = GL_UNSIGNED_SHORT;
+			break;
+		case 5:
+			dataPtr = (unsigned char*)boost::gil::interleaved_view_get_raw_data( *viewRgb16 );
+			internalFormat = 3;
+			width  = viewRgb16->width();
+			height = viewRgb16->height();
+			format = GL_RGB;
+			type   = GL_UNSIGNED_SHORT;
+			break;
+		case 6:
+			dataPtr = (unsigned char*)boost::gil::interleaved_view_get_raw_data( *viewRgba16 );
+			internalFormat = 4;
+			width  = viewRgba16->width();
+			height = viewRgba16->height();
+			format = GL_RGBA;
+			type   = GL_UNSIGNED_SHORT;
+			break;
+		case 7:
+			dataPtr = (unsigned char*)boost::gil::interleaved_view_get_raw_data( *viewGray32 );
+			internalFormat = 1;
+			width  = viewGray32->width();
+			height = viewGray32->height();
+			format = GL_LUMINANCE;
+			type   = GL_FLOAT;
+			break;
+		case 8:
+			dataPtr = (unsigned char*)boost::gil::interleaved_view_get_raw_data( *viewRgb32 );
+			internalFormat = 3;
+			width  = viewRgb32->width();
+			height = viewRgb32->height();
+			format = GL_RGB;
+			type   = GL_FLOAT;
+			break;
+		case 9:
+			dataPtr = (unsigned char*)boost::gil::interleaved_view_get_raw_data( *viewRgba32 );
+			internalFormat = 4;
+			width  = viewRgba32->width();
+			height = viewRgba32->height();
+			format = GL_RGBA;
+			type   = GL_FLOAT;
+			break;
+		case 10:
+			dataPtr = (unsigned char*)boost::gil::interleaved_view_get_raw_data( *viewGray32f );
+			internalFormat = 1;
+			width  = viewGray32f->width();
+			height = viewGray32f->height();
+			format = GL_LUMINANCE;
+			type   = GL_FLOAT;
+			break;
+		case 11:
+			dataPtr = (unsigned char*)boost::gil::interleaved_view_get_raw_data( *viewRgb32f );
+			internalFormat = 3;
+			width  = viewRgb32f->width();
+			height = viewRgb32f->height();
+			format = GL_RGB;
+			type   = GL_FLOAT;
+			break;
+		case 12:
+			dataPtr = (unsigned char*)boost::gil::interleaved_view_get_raw_data( *viewRgba32f );
+			internalFormat = 4;
+			width  = viewRgba32f->width();
+			height = viewRgba32f->height();
+			format = GL_RGBA;
+			type   = GL_FLOAT;
+			break;
+	}
+
+	
+	glTexImage2D( GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, type, dataPtr );
+	
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	
+	glEnable(GL_TEXTURE_2D);
+}
+
+void loadAlphaTexture( )
+{
+	unsigned char* dataPtr = NULL;
+	
+	GLint	internalFormat = 1;
+	size_t	width=0;
+	size_t	height=0;
+	GLenum	format = GL_RGBA;
+	GLenum	type = GL_UNSIGNED_BYTE;
+	switch( loadedTexture )
+	{
+
+		case 3:
+			dataPtr = (unsigned char*)boost::gil::interleaved_view_get_raw_data( *viewRgba8 );
+			internalFormat = 4;
+			width  = viewRgba8->width();
+			height = viewRgba8->height();
+			format = GL_RGBA;
+			type   = GL_UNSIGNED_BYTE;
+			break;
+		case 6:
+			dataPtr = (unsigned char*)boost::gil::interleaved_view_get_raw_data( *viewRgba16 );
+			internalFormat = 4;
+			width  = viewRgba16->width();
+			height = viewRgba16->height();
+			format = GL_RGBA;
+			type   = GL_UNSIGNED_SHORT;
+			break;
+
+		case 9:
+			dataPtr = (unsigned char*)boost::gil::interleaved_view_get_raw_data( *viewRgba32 );
+			internalFormat = 4;
+			width  = viewRgba32->width();
+			height = viewRgba32->height();
+			format = GL_RGBA;
+			type   = GL_FLOAT;
+			break;
+
+		case 12:
+			dataPtr = (unsigned char*)boost::gil::interleaved_view_get_raw_data( *viewRgba32f );
+			internalFormat = 4;
+			width  = viewRgba32f->width();
+			height = viewRgba32f->height();
+			format = GL_RGBA;
+			type   = GL_FLOAT;
+			break;
+		case 0:
+		case 1:
+		case 2:
+		case 4:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 11:
+			return;
+	}
+
+	
+	glTexImage2D( GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, type, dataPtr );
+	
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	
+	glEnable(GL_TEXTURE_2D);
+}
+
+void setTransfert( float red, float green, float blue, float alpha=1.f )
+{
+	switch( loadedTexture )
+	{
+		case 0 : /// undefined type
+		case 1 : /// Gray 8  bits
+		case 4 : /// Gray 16 bits
+		case 7 : /// Gray 32 bits
+		case 10: /// Gray 32 float
+			return;
+		case 2 : /// RGB  8  bits
+		case 5 : /// RGB  16 bits
+		case 8 : /// RGB  32 bits
+		case 11: /// RGB  32 float
+			glPixelTransferf( GL_RED_SCALE,   red );
+			glPixelTransferf( GL_GREEN_SCALE, green );
+			glPixelTransferf( GL_BLUE_SCALE,  blue );
+			break;
+		case 3 : /// RGBA 8  bits
+		case 6 : /// RGBA 16 bits
+		case 9 : /// RGBA 32 bits
+		case 12: /// RGBA 32 float
+			glPixelTransferf( GL_RED_SCALE,   red );
+			glPixelTransferf( GL_GREEN_SCALE, green );
+			glPixelTransferf( GL_BLUE_SCALE,  blue );
+			glPixelTransferf( GL_ALPHA_SCALE, alpha );
+			break;
+	}
+	reloadTexture( );
+}
+
+void showRedChannelTexture( )
+{
+	if( ! showRedChannel )
+	{
+		setTransfert( 1.f, 0.f, 0.f );
+		showRedChannel = true;
+		showGreenChannel = false;
+		showBlueChannel = false;
+		showAlphaChannel = false;
+	}
+	else
+	{
+		setTransfert( 1.f, 1.f, 1.f );
+		showRedChannel = false;
+	}
+	glutPostRedisplay ();
+}
+
+void showGreenChannelTexture( )
+{
+	if( ! showGreenChannel )
+	{
+		setTransfert( 0.f, 1.f, 0.f );
+		showRedChannel = false;
+		showGreenChannel = true;
+		showBlueChannel = false;
+		showAlphaChannel = false;
+	}
+	else
+	{
+		setTransfert( 1.f, 1.f, 1.f );
+		showGreenChannel = false;
+	}
+	glutPostRedisplay ();
+}
+
+void showBlueChannelTexture( )
+{
+	if( ! showBlueChannel )
+	{
+		setTransfert( 0.f, 0.f, 1.f );
+		showRedChannel = false;
+		showGreenChannel = false;
+		showBlueChannel = true;
+		showAlphaChannel = false;
+	}
+	else
+	{
+		setTransfert( 1.f, 1.f, 1.f );
+		showBlueChannel = false;
+	}
+	glutPostRedisplay ();
+}
+
+void showAlphaChannelTexture( )
+{
+	if( ! showAlphaChannel )
+	{
+		loadAlphaTexture( );
+		showRedChannel = false;
+		showGreenChannel = false;
+		showBlueChannel = false;
+		showAlphaChannel = true;
+	}
+	else
+	{
+		setTransfert( 1.f, 1.f, 1.f, 1.f );
+		showAlphaChannel = false;
+	}
+	glutPostRedisplay ();
+}
+
 void display()
 {
 	if( glutGetWindow() == 0 ) return;
@@ -374,6 +673,19 @@ void keyboard(unsigned char k, int x, int y)
 		case SPACEBAR:
 			glutDestroyWindow(windowID);
 			break;
+			
+		case 'r':
+			showRedChannelTexture( ); 
+			break;
+		case 'g':
+			showGreenChannelTexture( ); 
+			break;
+		case 'b':
+			showBlueChannelTexture( ); 
+			break;
+		case 'a':
+			showAlphaChannelTexture();
+			break;
 	}
 }
 
@@ -396,7 +708,6 @@ void mouse  ( int button, int state, int x, int y )
 	if( state == 0 && button == 0)
 	{
 		float data[4];
-		
 		switch( loadedTexture )
 		{
 			case 0 :
@@ -409,84 +720,96 @@ void mouse  ( int button, int state, int x, int y )
 										std::setw(9) << data[3] << std::endl;
 				break;
 			case 1 : // gray 8
-				std::cout << "at " <<	std::setw(4) << x << "," <<
-										std::setw(4) << y << ": " <<
-										std::setw(5) << (int) get_color( viewGray8[0](x,y), gray_color_t() ) << std::endl;
+				if( x < viewGray8->width() && y < viewGray8->height() )
+					std::cout << "at " <<	std::setw(4) << x << "," <<
+											std::setw(4) << y << ": " <<
+											std::setw(5) << (int) get_color( viewGray8[0](x,y), gray_color_t() ) << std::endl;
 				break;
 			case 2 : // rgb 8
-				std::cout << "at " <<	std::setw(4) << x << "," <<
-										std::setw(4) << y << ": " <<
-										std::setw(5) << (int) get_color( viewRgb8[0](x,y), red_t() ) << " " <<
-										std::setw(5) << (int) get_color( viewRgb8[0](x,y), green_t() ) << " " <<
-										std::setw(5) << (int) get_color( viewRgb8[0](x,y), blue_t() ) << std::endl;
+				if( x < viewRgb8->width() && y < viewRgb8->height() )
+					std::cout << "at " <<	std::setw(4) << x << "," <<
+											std::setw(4) << y << ": " <<
+											std::setw(5) << (int) get_color( viewRgb8[0](x,y), red_t() ) << " " <<
+											std::setw(5) << (int) get_color( viewRgb8[0](x,y), green_t() ) << " " <<
+											std::setw(5) << (int) get_color( viewRgb8[0](x,y), blue_t() ) << std::endl;
 				break;
 			case 3 : // rgba 8
-				std::cout << "at " <<	std::setw(4) << x << "," <<
-										std::setw(4) << y << ": " <<
-										std::setw(5) << (int) get_color( viewRgba8[0](x,y), red_t() ) << " " <<
-										std::setw(5) << (int) get_color( viewRgba8[0](x,y), green_t() ) << " " <<
-										std::setw(5) << (int) get_color( viewRgba8[0](x,y), blue_t() ) << " " <<
-										std::setw(5) << (int) get_color( viewRgba8[0](x,y), alpha_t() ) << std::endl;
+				if( x < viewRgba8->width() && y < viewRgba8->height() )
+					std::cout << "at " <<	std::setw(4) << x << "," <<
+											std::setw(4) << y << ": " <<
+											std::setw(5) << (int) get_color( viewRgba8[0](x,y), red_t() ) << " " <<
+											std::setw(5) << (int) get_color( viewRgba8[0](x,y), green_t() ) << " " <<
+											std::setw(5) << (int) get_color( viewRgba8[0](x,y), blue_t() ) << " " <<
+											std::setw(5) << (int) get_color( viewRgba8[0](x,y), alpha_t() ) << std::endl;
 				break;
 			case 4 :
-				std::cout << "at " <<	std::setw(4) << x << "," <<
-										std::setw(4) << y << ": " <<
-										std::setw(7) << get_color( viewGray16[0](x,y), gray_color_t() ) << std::endl;
+				if( x < viewRgb16->width() && y < viewRgb16->height() )
+					std::cout << "at " <<	std::setw(4) << x << "," <<
+											std::setw(4) << y << ": " <<
+											std::setw(7) << get_color( viewGray16[0](x,y), gray_color_t() ) << std::endl;
 				break;
 			case 5 :
-				std::cout << "at " <<	std::setw(4) << x << "," <<
-										std::setw(4) << y << ": " <<
-										std::setw(7) << get_color( viewRgb16[0](x,y), red_t() ) << " " <<
-										std::setw(7) << get_color( viewRgb16[0](x,y), green_t() ) << " " <<
-										std::setw(7) << get_color( viewRgb16[0](x,y), blue_t() ) << std::endl;
+				if( x < viewRgb16->width() && y < viewRgb16->height() )
+					std::cout << "at " <<	std::setw(4) << x << "," <<
+											std::setw(4) << y << ": " <<
+											std::setw(7) << get_color( viewRgb16[0](x,y), red_t() ) << " " <<
+											std::setw(7) << get_color( viewRgb16[0](x,y), green_t() ) << " " <<
+											std::setw(7) << get_color( viewRgb16[0](x,y), blue_t() ) << std::endl;
 				break;
 			case 6 :
-				std::cout << "at " <<	std::setw(4) << x << "," <<
-										std::setw(4) << y << ": " <<
-										std::setw(7) << get_color( viewRgba16[0](x,y), red_t() ) << " " <<
-										std::setw(7) << get_color( viewRgba16[0](x,y), green_t() ) << " " <<
-										std::setw(7) << get_color( viewRgba16[0](x,y), blue_t() ) << " " <<
-										std::setw(7) << get_color( viewRgba16[0](x,y), alpha_t() ) << std::endl;
+				if( x < viewRgba16->width() && y < viewRgba16->height() )
+					std::cout << "at " <<	std::setw(4) << x << "," <<
+											std::setw(4) << y << ": " <<
+											std::setw(7) << get_color( viewRgba16[0](x,y), red_t() ) << " " <<
+											std::setw(7) << get_color( viewRgba16[0](x,y), green_t() ) << " " <<
+											std::setw(7) << get_color( viewRgba16[0](x,y), blue_t() ) << " " <<
+											std::setw(7) << get_color( viewRgba16[0](x,y), alpha_t() ) << std::endl;
 				break;
 			case 7 :
-				std::cout << "at " <<	std::setw(4) << x << "," <<
-										std::setw(4) << y << ": " <<
-										std::setw(12) << get_color( viewGray32[0](x,y), gray_color_t() ) << std::endl;
+				if( x < viewGray32->width() && y < viewGray32->height() )
+					std::cout << "at " <<	std::setw(4) << x << "," <<
+											std::setw(4) << y << ": " <<
+											std::setw(12) << get_color( viewGray32[0](x,y), gray_color_t() ) << std::endl;
 				break;
 			case 8 :
-				std::cout << "at " <<	std::setw(4) << x << "," <<
-										std::setw(4) << y << ": " <<
-										std::setw(12) << get_color( viewRgb32[0](x,y), red_t() ) << " " <<
-										std::setw(12) << get_color( viewRgb32[0](x,y), green_t() ) << " " <<
-										std::setw(12) << get_color( viewRgb32[0](x,y), blue_t() ) << std::endl;
+				if( x < viewRgb32->width() && y < viewRgb32->height() )
+					std::cout << "at " <<	std::setw(4) << x << "," <<
+											std::setw(4) << y << ": " <<
+											std::setw(12) << get_color( viewRgb32[0](x,y), red_t() ) << " " <<
+											std::setw(12) << get_color( viewRgb32[0](x,y), green_t() ) << " " <<
+											std::setw(12) << get_color( viewRgb32[0](x,y), blue_t() ) << std::endl;
 				break;
 			case 9 :
-				std::cout << "at " <<	std::setw(4) << x << "," <<
-										std::setw(4) << y << ": " <<
-										std::setw(12) << get_color( viewRgba32[0](x,y), red_t() ) << " " <<
-										std::setw(12) << get_color( viewRgba32[0](x,y), green_t() ) << " " <<
-										std::setw(12) << get_color( viewRgba32[0](x,y), blue_t() ) << " " <<
-										std::setw(12) << get_color( viewRgba32[0](x,y), alpha_t() ) << std::endl;
+				if( x < viewRgba32->width() && y < viewRgba32->height() )
+					std::cout << "at " <<	std::setw(4) << x << "," <<
+											std::setw(4) << y << ": " <<
+											std::setw(12) << get_color( viewRgba32[0](x,y), red_t() ) << " " <<
+											std::setw(12) << get_color( viewRgba32[0](x,y), green_t() ) << " " <<
+											std::setw(12) << get_color( viewRgba32[0](x,y), blue_t() ) << " " <<
+											std::setw(12) << get_color( viewRgba32[0](x,y), alpha_t() ) << std::endl;
 				break;
 			case 10:
-				std::cout << "at " <<	std::setw(4) << x << "," <<
-										std::setw(4) << y << ": " <<
-										std::setw(12) << get_color( viewGray32f[0](x,y), gray_color_t() ) << std::endl;
+				if( x < viewGray32f->width() && y < viewGray32f->height() )
+					std::cout << "at " <<	std::setw(4) << x << "," <<
+											std::setw(4) << y << ": " <<
+											std::setw(12) << get_color( viewGray32f[0](x,y), gray_color_t() ) << std::endl;
 				break;
 			case 11:
-				std::cout << "at " <<	std::setw(4) << x << "," <<
-										std::setw(4) << y << ": " <<
-										std::setw(12) << get_color( viewRgb32f[0](x,y), red_t() ) << " " <<
-										std::setw(12) << get_color( viewRgb32f[0](x,y), green_t() ) << " " <<
-										std::setw(12) << get_color( viewRgb32f[0](x,y), blue_t() ) << std::endl;
+				if( x < viewRgb32f->width() && y < viewRgb32f->height() )
+					std::cout << "at " <<	std::setw(4) << x << "," <<
+											std::setw(4) << y << ": " <<
+											std::setw(12) << get_color( viewRgb32f[0](x,y), red_t() ) << " " <<
+											std::setw(12) << get_color( viewRgb32f[0](x,y), green_t() ) << " " <<
+											std::setw(12) << get_color( viewRgb32f[0](x,y), blue_t() ) << std::endl;
 				break;
 			case 12:
-				std::cout << "at " <<	std::setw(4) << x << "," <<
-										std::setw(4) << y << ": " <<
-										std::setw(12) << get_color( viewRgba32f[0](x,y), red_t() ) << " " <<
-										std::setw(12) << get_color( viewRgba32f[0](x,y), green_t() ) << " " <<
-										std::setw(12) << get_color( viewRgba32f[0](x,y), blue_t() ) << " " <<
-										std::setw(12) << get_color( viewRgba32f[0](x,y), alpha_t() ) << std::endl;
+				if( x < viewRgba32f->width() && y < viewRgba32f->height() )
+					std::cout << "at " <<	std::setw(4) << x << "," <<
+											std::setw(4) << y << ": " <<
+											std::setw(12) << get_color( viewRgba32f[0](x,y), red_t() ) << " " <<
+											std::setw(12) << get_color( viewRgba32f[0](x,y), green_t() ) << " " <<
+											std::setw(12) << get_color( viewRgba32f[0](x,y), blue_t() ) << " " <<
+											std::setw(12) << get_color( viewRgba32f[0](x,y), alpha_t() ) << std::endl;
 				break;
 		}
 	}
