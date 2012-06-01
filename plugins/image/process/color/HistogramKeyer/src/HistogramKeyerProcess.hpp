8f2b3bb078394f32e67124a76c842ab915369572
Fabien Castan <fabien.castan@free.fr>
2012-06-01 09:02:12 +0200

    minor compilation fix

--- plugins/image/process/color/HistogramKeyer/src/HistogramKeyerProcess.hpp
+++ plugins/image/process/color/HistogramKeyer/src/HistogramKeyerProcess.hpp
@@ -28,7 +28,7 @@ struct Compute_alpha_pixel
 
         rgb32f_pixel_t convert_to_rgb;
         color_convert( p, convert_to_rgb ); // first step : p to rgb_pixel
-		terry::pixel_clamp<rgb32f_pixel_t>(0, 1)( convert_to_rgb, convert_to_rgb );
+        terry::default_clamp_converter<rgb32f_pixel_t>(0, 1)( convert_to_rgb, convert_to_rgb );
         hsl32f_pixel_t hsl_pix;
         color_convert(convert_to_rgb,hsl_pix ); // second step : rgb_pixel to hsl_pixel
         
