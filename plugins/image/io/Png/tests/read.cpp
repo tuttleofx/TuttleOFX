4a6754761d4c53970e67de2886f1d2d960f3f079
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2012-07-17 18:38:33 +0200

    stating to add unit test for IO plugins

--- plugins/image/io/Png/tests/read.cpp
+++ plugins/image/io/Png/tests/read.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#define BOOST_TEST_MODULE plugin_Exr
+#define BOOST_TEST_MODULE plugin_Png
 #include <boost/test/unit_test.hpp>
 
 #include <tuttle/host/Graph.hpp>
@@ -15,34 +15,35 @@
  */
 
 using namespace boost::unit_test;
+using namespace tuttle::host;
 
-BOOST_AUTO_TEST_SUITE( plugin_Exr_reader )
+BOOST_AUTO_TEST_SUITE( plugin_Png_reader )
+
+BOOST_AUTO_TEST_CASE( loading_openfx_plugins )
+{
+	TUTTLE_COUT( "-------- LOADING OPENFX PLUGINS --------" );
+	Core::instance().getPluginCache().addDirectoryToPath( BOOST_PP_STRINGIZE(TUTTLE_PLUGIN_PATH) );
+	Core::instance().preload();
+	TUTTLE_COUT( Core::instance().getImageEffectPluginCache() );
+	TUTTLE_COUT( "----------------- DONE -----------------" );
+}
 
 BOOST_AUTO_TEST_CASE( process_reader )
 {
-	using namespace tuttle::host;
-	TUTTLE_TCOUT( "__________________________________________________0" );
-	Core::instance().getPluginCache().addDirectoryToPath( BOOST_PP_STRINGIZE(TUTTLE_PLUGIN_PATH) );
-	Core::instance().preload();
-
-//	TUTTLE_TCOUT( Core::instance().getImageEffectPluginCache() );
-
+	TUTTLE_COUT( "******** PROCESS READER PNG ********" );
 	Graph g;
 
-	TUTTLE_TCOUT( "__________________________________________________1" );
-	Graph::Node& read = g.createNode( "tuttle.exrreader" );
+	TUTTLE_COUT( "--> PLUGINS CREATION" );
+	Graph::Node& read = g.createNode( "tuttle.pngreader" );
 
-	TUTTLE_TCOUT( "__________________________________________________3" );
+	TUTTLE_COUT( "--> PLUGINS CONFIGURATION" );
 
-	read.getParam( "filename" ).setValue( "data/input.exr" );
-
-	TUTTLE_TCOUT( "__________________________________________________4" );
-//	g.connect( read, write );
-
-	TUTTLE_TCOUT( "__________________________________________________5" );
+	read.getParam( "filename" ).setValue( "data/file.png" );
+	
+	TUTTLE_COUT( "--> GRAPH PROCESSING" );
 	boost::posix_time::ptime t1a(boost::posix_time::microsec_clock::local_time());
 	ComputeOptions options;
-        options._returnBuffers = true;
+	options._returnBuffers = true;
 	memory::MemoryCache res0 = g.compute( read, options );
 	boost::posix_time::ptime t2a(boost::posix_time::microsec_clock::local_time());
 
@@ -67,20 +68,16 @@ BOOST_AUTO_TEST_CASE( process_reader )
 
 BOOST_AUTO_TEST_CASE( process_nofile )
 {
-	using namespace tuttle::host;
-	TUTTLE_TCOUT( "__________________________________________________0" );
+	TUTTLE_COUT( "******** PROCESS READER PNG NO FILE ********" );
 	Graph g;
 
-	TUTTLE_TCOUT( "__________________________________________________1" );
-	Graph::Node& read = g.createNode( "tuttle.exrreader" );
+	TUTTLE_COUT( "--> PLUGINS CREATION" );
+	Graph::Node& read = g.createNode( "tuttle.pngreader" );
 
-	TUTTLE_TCOUT( "__________________________________________________3" );
-	read.getParam( "filename" ).setValue( "data/no-such-file.exr" );
+	TUTTLE_COUT( "--> PLUGINS CONFIGURATION" );
+	read.getParam( "filename" ).setValue( "data/no-such-file.png" );
 
-	TUTTLE_TCOUT( "__________________________________________________4" );
-//	g.connect( read, write );
-
-	TUTTLE_TCOUT( "__________________________________________________5" );
+	TUTTLE_COUT( "---> GRAPH PROCESSING" );
 	BOOST_REQUIRE_THROW( g.compute( read ), boost::exception );
 }
 
