44a149fdd907dd8b8ffabae18fc4032c8e5728a8
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2012-07-23 11:27:45 +0200

    TurboJpeg: adding optimized reader/writer of Jpeg files #21

---
+++ plugins/image/io/TurboJpeg/src/reader/TurboJpegReaderAlgorithm.hpp
@@ -0,0 +1,14 @@
+#ifndef _TUTTLE_PLUGIN_TURBOJPEG_READER_ALGORITHM_HPP_
+#define _TUTTLE_PLUGIN_TURBOJPEG_READER_ALGORITHM_HPP_
+
+namespace tuttle {
+namespace plugin {
+namespace turboJpeg {
+namespace reader {
+
+}
+}
+}
+}
+
+#endif
