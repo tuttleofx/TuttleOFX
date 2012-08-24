e5a12c07067e2e4ed116babe8c0d9571ffd28d07
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2012-08-24 15:58:22 +0200

    change __LINUX__ to __UNIX__ macro detection

--- libraries/tuttle/src/tuttle/common/utils/color.hpp
+++ libraries/tuttle/src/tuttle/common/utils/color.hpp
@@ -6,7 +6,7 @@
 namespace tuttle {
 namespace common {
 
-#ifdef __LINUX__
+#ifdef __UNIX__
 static const std::string kColorBlack    ( "\E[1;30m" );
 static const std::string kColorWhite    ( "\E[1;37m" );
 static const std::string kColorBlue     ( "\E[1;34m" );
