a46c991d5e97c3371de94b5824ace58d1ed9fe57
Fabien Castan <fabien.castan@free.fr>
2012-08-26 17:21:14 +0200

    tuttleHost: numpy binding works with RGB and RGBA images
    The binding doesn't support function overloading in the particular case of parameters replacement.
    Replace (ptr, int, int) -> (array2D)
    Replace (ptr, int, int, int) -> (array3D)
    
    Issue #56

--- applications/example/python/numpyInputBuffer.py
+++ applications/example/python/numpyInputBuffer.py
@@ -6,25 +6,16 @@ tuttle.Core.instance().preload()
 g = tuttle.Graph()
 
 ib = g.createInputBuffer()
-x = numpy.array([[.9, .1, .9], [.8, .2, .9]], numpy.float32)
-ib.setGrayImage( x )
+# numpy array from an image
+img = numpy.asarray(Image.open('data/input.jpg'))
+ii.set2DArrayBuffer( img )
+# generated numpy array
+#x = numpy.array([[.9, .1, .9], [.8, .2, .9]], numpy.float32)
+#ib.set2DArrayBuffer( x )
 
 w = g.createNode("tuttle.pngwriter")
 w.getParam("filename").setValue("foo.png")
 
-g.connect( ib, w )
+g.connect( ib.node(), w )
 g.compute( w )
-
-
-
-
-
-
-
-
-
-#a = [[1,2,3,4],[1,2,3,4]]
-#ib.setRGBAImage( a )
-#a = numpy.array([1,2,3],'f')
-#ib.setGrayImage( a )
 
