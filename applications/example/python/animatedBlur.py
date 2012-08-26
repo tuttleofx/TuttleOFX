c30816aa14d6a16a7b7d9ed9ded7e11653f306ef
Fabien Castan <fabien.castan@free.fr>
2012-08-27 01:22:48 +0200

    add param animation example

---
+++ applications/example/python/animatedBlur.py
@@ -0,0 +1,24 @@
+from pyTuttle import tuttle
+
+tuttle.Core.instance().preload()
+
+g = tuttle.Graph()
+read = g.createNode( "tuttle.pngreader" )
+blur = g.createNode( "tuttle.blur" )
+write = g.createNode( "tuttle.pngwriter" )
+
+read.getParam("filename").setValue( "data/input.png" )
+blur.getParam( "size" ).setValueAtTime( 1.0, 80.0, 40.0 )
+blur.getParam( "size" ).setValueAtTime( 5.0, 0.0, 0.0 )
+
+s = blur.getParam("size")
+for i in xrange(0, 6*5):
+    t = i/5.0 # step is 0.2
+    print "blur size at ", t, ":", \
+        "(", s.getDoubleValueAtTimeAndIndex(t, 0), ", ", \
+             s.getDoubleValueAtTimeAndIndex(t, 1), ")"
+
+write.getParam("filename").setValue( "data/output_####.png" )
+
+g.connect( [read,blur,write] )
+g.compute( write, tuttle.ComputeOptions( 1, 5 ) )
