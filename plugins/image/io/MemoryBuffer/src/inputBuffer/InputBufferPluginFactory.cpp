7041edbda5d6516f0d6a5de499f0d7fe30d8ab4d
Fabien Castan <fabien.castan@free.fr>
2012-09-27 09:00:55 +0200

    InputBuffer: externalize fake pointer param
    * put conversion functions between string and pointer into a separate file
    * some renaming

--- plugins/image/io/MemoryBuffer/src/inputBuffer/InputBufferPluginFactory.cpp
+++ plugins/image/io/MemoryBuffer/src/inputBuffer/InputBufferPluginFactory.cpp
@@ -96,17 +96,17 @@ void InputBufferPluginFactory::describeInContext( OFX::ImageEffectDescriptor& de
 	callbackPointer->setAnimates( false );
 	callbackPointer->setDefault( "" );
 	
-	OFX::StringParamDescriptor* callbackCustomData = desc.defineStringParam( kParamInputCallbackCustomData );
-	callbackCustomData->setLabel( "Callback Custom Data Pointer" );
-	callbackCustomData->setHint(
+	OFX::StringParamDescriptor* customData = desc.defineStringParam( kParamInputCustomData );
+	customData->setLabel( "Callback Custom Data Pointer" );
+	customData->setHint(
 		"This parameter represent a pointer to a custom data which is given to the callback function.\n"
 		"WARNING:\n"
 		" - Your application could crash if you set an invalid value here.\n"
 		);
-	callbackCustomData->setCacheInvalidation( OFX::eCacheInvalidateValueAll );
-	callbackCustomData->setIsPersistant( false );
-	callbackCustomData->setAnimates( false );
-	callbackCustomData->setDefault( "" );
+	customData->setCacheInvalidation( OFX::eCacheInvalidateValueAll );
+	customData->setIsPersistant( false );
+	customData->setAnimates( false );
+	customData->setDefault( "" );
 	
 	
 	OFX::Int2DParamDescriptor* imgSize = desc.defineInt2DParam( kParamSize );
