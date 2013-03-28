5b5b131ba9d2131dfdb18b8aa50bc2bee04a7602
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2013-03-28 17:47:14 +0100

    using temporary directory for BitDepth test

--- libraries/tuttle/pyTest/testBitDepth.py
+++ libraries/tuttle/pyTest/testBitDepth.py
@@ -9,10 +9,10 @@ def setUp():
 
 @raises(Exception)
 def testIncompatibleBitDepth():
-	
+	filepath = NamedTemporaryFile( preffix="bitDepthTest-", suffix=".png" )
 	compute( [
 		NodeInit( "tuttle.checkerboard", size=[50,50] ),
 		NodeInit( "tuttle.blur", size=[0.03, 0.05] ), # only supports 32f
 		NodeInit( "tuttle.swscale", height=500 ), # only supports 8ui
-		NodeInit( "tuttle.pngwriter", filename=".tests/output.png" ),
+		NodeInit( "tuttle.pngwriter", filename=filepath.name ),
 		] )
