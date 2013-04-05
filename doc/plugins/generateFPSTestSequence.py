40c5c5c312681b63765b16809268da38163a34c7
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2013-04-05 18:39:12 +0200

    FFMpeg tests: add some more encodign tests

--- doc/plugins/generateFPSTestSequence.py
+++ doc/plugins/generateFPSTestSequence.py
@@ -7,37 +7,39 @@ from movie import *
 def setUp():
 	core().preload( False )
 
-def testFPSGenerator():
+def testFPSGenerator( ):
 	generate( "fps_test_", 50 )
 	
-def testEncodeSequencePng():
+def encode( outFilename, *orderedParams, **namedParams ):
 	graph = Graph()
 	
 	read = graph.createNode( "tuttle.pngreader", filename=os.path.join( os.environ["DOC_DIR"], "images", "fps_test_#####.png" ) )
-	write = graph.createNode( "tuttle.ffmpegwriter", filename=os.path.join( os.environ["DOC_DIR"], "videos", "fps_test_png.avi" ), format="avi", videoCodec="png" )
+	write = graph.createNode( "tuttle.ffmpegwriter", filename=os.path.join( os.environ["DOC_DIR"], "videos", outFilename ), *orderedParams, **namedParams )
 	graph.connect( [read, write] )
 	graph.compute( write )
 
-def testEncodeSequenceProres():
-	graph = Graph()
+def testEncodeSequencePng():
+	encode( "fps_test_png.avi" , format="avi", videoCodec="png" )
+
+def testEncodeSequenceMpeg2():
+	encode( "MPEG-2_I-frame_only_Highest_Quality_Encoding.m2v" , format="mpeg2video", videoCodec="mpeg2video", videoPixelFormat="yuv422p", qmin=1, mpeg2video_intra_vlc=1 )
+	# missing parameter qscale=1
 	
-	read = graph.createNode( "tuttle.pngreader", filename=os.path.join( os.environ["DOC_DIR"], "images", "fps_test_#####.png" ) )
-	write = graph.createNode( "tuttle.ffmpegwriter", filename=os.path.join( os.environ["DOC_DIR"], "videos", "fps_test.mov" ), format="mov", videoCodec="prores" )
-	graph.connect( [read, write] )
-	graph.compute( write )
+	encode( "MPEG-2_Long_GOP_Encoding.m2v" , format="mpeg2video", videoCodec="mpeg2video", b=5000000 , g=300, bf=2, b_strategy=1 )
+	
+def testEncodeSequenceProres():
+	encode( "fps_test_mov.mov" , format="mov", videoCodec="prores" )
 
 def testEncodeSequenceMJpeg():
-	graph = Graph()
-	
-	read = graph.createNode( "tuttle.pngreader", filename=os.path.join( os.environ["DOC_DIR"], "images", "fps_test_#####.png" ) )
-	write = graph.createNode( "tuttle.ffmpegwriter", filename=os.path.join( os.environ["DOC_DIR"], "videos", "fps_test_mjpeg.avi" ), format="avi", videoCodec="mjpeg", qmin=7, qmax=1 )
-	graph.connect( [read, write] )
-	graph.compute( write )
-	
+	encode( "fps_test_mjpeg_qmin_7_qmax_1.avi" , format="avi", videoCodec="mjpeg", qmin=7, qmax=1 )
+
 def testEncodeSequenceWebm():
-	graph = Graph()
+	encode( "fps_test_libvpx_b_1000000.webm" , format="webm", videoCodec="libvpx", b=1000000 )
 	
-	read = graph.createNode( "tuttle.pngreader", filename=os.path.join( os.environ["DOC_DIR"], "images", "fps_test_#####.png" ) )
-	write = graph.createNode( "tuttle.ffmpegwriter", filename=os.path.join( os.environ["DOC_DIR"], "videos", "fps_test_mjpeg.webm" ), format="webm", videoCodec="libvpx", b=1000000 )
-	graph.connect( [read, write] )
-	graph.compute( write )
+def testEncodeSequenceRawVideo():
+	encode( "fps_test_yuvRaw.y4m" , format="yuv4mpegpipe", videoCodec="rawvideo" )
+
+#def testEncodeSequenceH264():
+#	encode( "fps_test_x264.mov" , format="mov", videoCodec="libx264" )
+#	encode( "fps_test_x264_veryfast.mov" , format="mov", videoCodec="libx264", libx264_preset="veryfast" )
+#	encode( "fps_test_x264_slower.mov" , format="mov", videoCodec="libx264", libx264_preset="slower" )
