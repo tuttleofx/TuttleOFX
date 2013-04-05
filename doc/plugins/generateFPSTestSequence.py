0e8da839e1d93f0e3b901d9a9200474a8566d477
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2013-04-05 19:59:04 +0200

    FFmpeg tests: adding fps, range, pixel aspect ratio

--- doc/plugins/generateFPSTestSequence.py
+++ doc/plugins/generateFPSTestSequence.py
@@ -1,8 +1,10 @@
-# scons: Text Png FFMpeg
+# scons: Text Png FFMpeg Diff
 import os
 
 from pyTuttle.tuttle import *
 from movie import *
+
+from nose.tools import *
 
 def setUp():
 	core().preload( False )
@@ -18,26 +20,94 @@ def encode( outFilename, *orderedParams, **namedParams ):
 	graph.connect( [read, write] )
 	graph.compute( write )
 
+def checkVideoProperties( video, fps, frameDuration, pixelAspectRatio ):
+	graph = Graph()
+	readVideo = graph.createNode( "tuttle.ffmpegreader", filename=os.path.join( os.environ["DOC_DIR"], "videos", video ) ).asImageEffectNode()
+	
+	graph.setup()
+	td = readVideo.getTimeDomain()
+	framerate = readVideo.getOutputFrameRate()
+	pixelAspectRatio = readVideo.getOutputPixelAspectRatio()
+
+	print "time domain: ", td.min, td.max
+	print "framerate: ", framerate
+	print "pixel aspect ratio: ", pixelAspectRatio
+	
+	assert_equal( framerate, fps )
+	assert_equal( td.min, 0.0 )
+	assert_equal( td.max, frameDuration )
+	assert_equal( pixelAspectRatio, pixelAspectRatio )
+
+def check( video, sequence ):
+	graph = Graph()
+	
+	readSequence = graph.createNode( "tuttle.pngreader", filename=os.path.join( os.environ["DOC_DIR"], "images", sequence ) )
+	readVideo    = graph.createNode( "tuttle.ffmpegreader", filename=os.path.join( os.environ["DOC_DIR"], "videos", video ) )
+	diff         = graph.createNode( "tuttle.diff" ).asImageEffectNode()
+	
+	graph.connect( readSequence, diff.getClip("SourceA") )
+	graph.connect( readVideo, diff.getClip("SourceB") )
+	
+	graph.setup()
+	td = diff.getTimeDomain()
+
+	assert td.min == 0.0
+	assert td.max == 49.0
+
+	graph.compute( diff )
+	
+	param = diff.getParam("quality")
+	
+	print "diff = ", param.getDoubleValueAtIndex(0), param.getDoubleValueAtIndex(1), param.getDoubleValueAtIndex(2), param.getDoubleValueAtIndex(3)
+	
+	assert_equal( 0.0, param.getDoubleValueAtIndex(0) )
+	assert_equal( 0.0, param.getDoubleValueAtIndex(1) )
+	assert_equal( 0.0, param.getDoubleValueAtIndex(2) )
+	assert_equal( 0.0, param.getDoubleValueAtIndex(3) )
+
+
 def testEncodeSequencePng():
-	encode( "fps_test_png.avi" , format="avi", videoCodec="png" )
+	outputFilename = "fps_test_png.avi"
+	encode( outputFilename , format="avi", videoCodec="png" )
+	checkVideoProperties( outputFilename, 25.0, 49.0, 1.0 )
+	check( outputFilename, "fps_test_#####.png" )
+
+def testEncodeSequenceMpeg2Intra():
+	outputFilename = "MPEG-2_I-frame_only_Highest_Quality_Encoding.m2v"
+	encode( outputFilename , format="mpeg2video", videoCodec="mpeg2video", videoPixelFormat="yuv422p", qmin=1, mpeg2video_intra_vlc=1 )
+	checkVideoProperties( outputFilename, 25.0, 49.0, 1.0 )
+	check( outputFilename, "fps_test_#####.png" )
+	# missing parameter qscale=1
 
 def testEncodeSequenceMpeg2():
-	encode( "MPEG-2_I-frame_only_Highest_Quality_Encoding.m2v" , format="mpeg2video", videoCodec="mpeg2video", videoPixelFormat="yuv422p", qmin=1, mpeg2video_intra_vlc=1 )
-	# missing parameter qscale=1
-	
-	encode( "MPEG-2_Long_GOP_Encoding.m2v" , format="mpeg2video", videoCodec="mpeg2video", b=5000000 , g=300, bf=2, b_strategy=1 )
+	outputFilename = "MPEG-2_Long_GOP_Encoding.m2v"
+	encode( outputFilename , format="mpeg2video", videoCodec="mpeg2video", b=5000000 , g=300, bf=2, b_strategy=1 )
+	checkVideoProperties( outputFilename, 25.0, 49.0, 1.0 )
+	check( outputFilename, "fps_test_#####.png" )
 	
 def testEncodeSequenceProres():
-	encode( "fps_test_mov.mov" , format="mov", videoCodec="prores" )
+	outputFilename = "fps_test_mov.mov"
+	encode( outputFilename , format="mov", videoCodec="prores" )
+	checkVideoProperties( outputFilename, 25.0, 49.0, 1.0 )
+	check( outputFilename, "fps_test_#####.png" )
 
 def testEncodeSequenceMJpeg():
-	encode( "fps_test_mjpeg_qmin_7_qmax_1.avi" , format="avi", videoCodec="mjpeg", qmin=7, qmax=1 )
+	outputFilename = "fps_test_mjpeg_qmin_7_qmax_1.avi"
+	encode( outputFilename , format="avi", videoCodec="mjpeg", qmin=7, qmax=1 )
+	checkVideoProperties( outputFilename, 25.0, 49.0, 1.0 )
+	check( outputFilename, "fps_test_#####.png" )
 
 def testEncodeSequenceWebm():
-	encode( "fps_test_libvpx_b_1000000.webm" , format="webm", videoCodec="libvpx", b=1000000 )
+	outputFilename = "fps_test_libvpx_b_1000000.webm"
+	encode( outputFilename , format="webm", videoCodec="libvpx", b=1000000 )
+	checkVideoProperties( outputFilename, 25.0, 49.0, 1.0 )
+	check( outputFilename, "fps_test_#####.png" )
 	
 def testEncodeSequenceRawVideo():
-	encode( "fps_test_yuvRaw.y4m" , format="yuv4mpegpipe", videoCodec="rawvideo" )
+	outputFilename = "fps_test_yuvRaw.y4m"
+	encode( outputFilename , format="yuv4mpegpipe", videoCodec="rawvideo" )
+	checkVideoProperties( outputFilename, 25.0, 49.0, 1.0 )
+	check( outputFilename, "fps_test_#####.png" )
 
 #def testEncodeSequenceH264():
 #	encode( "fps_test_x264.mov" , format="mov", videoCodec="libx264" )
