45898a708cc7fedb9f7bffd84661997b0d6c5c6b
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2013-04-15 17:49:11 +0200

    adding comments on python code for documentation issue #194

--- doc/plugins/movie/movieGenerator.py
+++ doc/plugins/movie/movieGenerator.py
@@ -1,16 +1,19 @@
 import os
 from pyTuttle.tuttle import *
 
-def generate( prefix, length ):
+# generate a sequence test with number_of_frames frames, burned with the frame index
+# the sequence will be placed in the documentation directory (DOC_DIR)
+# ans the sequence will named: DOC_DIR/images/prefix#####.png
+def generate( prefix, number_of_frames ):
 
-	for i in range( length ):
+	for i in range( number_of_frames ):
 		graph = Graph()
 		text = graph.createNode( "tuttle.text", vAlign="center", font="teen", bold=1, color=[1,1,1,1], format="PAL", textSize=50, text=str(i) )
 		write = graph.createNode( "tuttle.pngwriter", filename=os.path.join( os.environ["DOC_DIR"], "images", prefix + str(i).zfill(5) + ".png" ) )
 		graph.connect( [text, write] )
 		graph.compute( write )
 		
-	for i in range( length ):
+	for i in range( number_of_frames ):
 		graph = Graph()
 		text = graph.createNode( "tuttle.text", vAlign="center", font="teen", bold=1, color=[1,1,1,1], format="HD", textSize=50, text=str(i) )
 		write = graph.createNode( "tuttle.pngwriter", filename=os.path.join( os.environ["DOC_DIR"], "images", prefix + "hd_" + str(i).zfill(5) + ".png" ) )
