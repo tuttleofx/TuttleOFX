20c7494428eabdedbb78e863e061eb2c01dc3fe7
Fabien Castan <fabien.castan@free.fr>
2013-11-15 20:17:34 +0100

    ofx: add utility functions

--- libraries/tuttle/src/tuttle/common/ofx/utilities.hpp
+++ libraries/tuttle/src/tuttle/common/ofx/utilities.hpp
@@ -6,6 +6,8 @@
 #include <ofxCore.h>
 #include <ofxImageEffect.h>
 
+#include <boost/numeric/conversion/cast.hpp>
+
 #include <string>
 #include <vector>
 
@@ -70,6 +72,19 @@ inline OfxRectD clamp( const OfxRectD& v,
 	return r;
 }
 
+/// clamp the rect in v to the given bounds
+inline OfxRectI clamp( const OfxRectI& v,
+                       const OfxRectI& bounds )
+{
+	OfxRectI r;
+
+	r.x1 = clamp( v.x1, bounds.x1, bounds.x2 );
+	r.x2 = clamp( v.x2, bounds.x1, bounds.x2 );
+	r.y1 = clamp( v.y1, bounds.y1, bounds.y2 );
+	r.y2 = clamp( v.y2, bounds.y1, bounds.y2 );
+	return r;
+}
+
 /// get the union of the two rects
 inline OfxRectD rectUnion( const OfxRectD& a,
                            const OfxRectD& b )
@@ -81,6 +96,46 @@ inline OfxRectD rectUnion( const OfxRectD& a,
 	r.y1 = minimum( a.y1, b.y1 );
 	r.y2 = maximum( a.y2, b.y2 );
 	return r;
+}
+
+inline bool isEmpty( const OfxRectD& r )
+{
+	if( r.x2 - r.x1 <= 0.0 )
+		return true;
+	if( r.y2 - r.y1 <= 0.0 )
+		return true;
+	return false;
+}
+
+inline bool isEmpty( const OfxRectI& r )
+{
+	if( r.x2 - r.x1 <= 0 )
+		return true;
+	if( r.y2 - r.y1 <= 0 )
+		return true;
+	return false;
+}
+
+/// create an infinite rectangle
+inline OfxRectD infiniteRectD()
+{
+	OfxRectD rect;
+	rect.x1 = kOfxFlagInfiniteMin;
+	rect.y1 = kOfxFlagInfiniteMin;
+	rect.x2 = kOfxFlagInfiniteMax;
+	rect.y2 = kOfxFlagInfiniteMax;
+	return rect;
+}
+
+/// Convert OfxRectD to OfxRectI
+inline OfxRectI rectDToRectI( const OfxRectD& rectD )
+{
+	OfxRectI rectI;
+	rectI.x1 = boost::numeric_cast<int>( std::floor( rectD.x1 ) );
+	rectI.y1 = boost::numeric_cast<int>( std::floor( rectD.y1 ) );
+	rectI.x2 = boost::numeric_cast<int>( std::ceil( rectD.x2 ) );
+	rectI.y2 = boost::numeric_cast<int>( std::ceil( rectD.y2 ) );
+	return rectI;
 }
 
 }
