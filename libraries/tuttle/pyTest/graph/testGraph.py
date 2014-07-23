0644359a8e4b2a9eddcb810652f2728037e62dec
Clement Champetier <cnt@mikrosimage.eu>
2014-07-23 12:02:53 +0200

    pyTest: add testDeleteUnconnectedNodes

--- libraries/tuttle/pyTest/graph/testGraph.py
+++ libraries/tuttle/pyTest/graph/testGraph.py
@@ -79,3 +79,35 @@ def testClearGraph():
 	lensdistort  = g.createNode( "tuttle.timeshift", 0 ).asImageEffectNode()
 
 	assert_equals(g.getNbNodes(), 2)
+
+def testDeleteUnconnectedNodes():
+
+	g = tuttle.Graph()
+	n = [
+			tuttle.NodeInit( "tuttle.oiioreader", filename="TuttleOFX-data/image/openexr/DisplayWindow/t##.exr" ),
+			tuttle.NodeInit( "tuttle.invert" ),
+			tuttle.NodeInit( "tuttle.timeshift", 12 ),
+			tuttle.NodeInit( "tuttle.gamma", master=.5 ),
+			tuttle.NodeInit( "tuttle.timeshift", 0 ),
+			tuttle.NodeInit( "tuttle.jpegwriter", filename=".tests/fromExr/output-####.jpg" ),
+		]
+	nodes = g.addConnectedNodes(n)
+
+	assert_equals(g.getNbNodes(), 6)
+	assert_equals(g.getNbConnections(), 5)
+
+	g.deleteUnconnectedNodes()
+
+	assert_equals(g.getNbNodes(), 6)
+	assert_equals(g.getNbConnections(), 5)
+
+	checkerboard = g.createNode( "tuttle.gamma", master=.5 ).asImageEffectNode()
+	lensdistort  = g.createNode( "tuttle.timeshift", 0 ).asImageEffectNode()
+
+	assert_equals(g.getNbNodes(), 8)
+	assert_equals(g.getNbConnections(), 5)
+
+	g.deleteUnconnectedNodes()
+
+	assert_equals(g.getNbNodes(), 6)
+	assert_equals(g.getNbConnections(), 5)
