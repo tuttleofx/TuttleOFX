d9c306e713353ef54fa2ef8eab893cf84f86a6f5
Cyril PICHARD <pih@devpc17.mikros.int>
2014-08-06 18:08:40 +0200

    cmake build system tests

---
+++ cmake/UseOFXPP.cmake
@@ -0,0 +1,5 @@
+# Uses custom ofx c++ wrapper
+
+
+include_directories(${PROJECT_SOURCE_DIR}/libraries/openfxHack/include)
+include_directories(${PROJECT_SOURCE_DIR}/libraries/openfxHack/Support/include)
---
+++ cmake/UseTerry.cmake
@@ -0,0 +1,4 @@
+
+# Use terry image library
+# include(UseTerry)
+include_directories(${PROJECT_SOURCE_DIR}/libraries/terry/src)
