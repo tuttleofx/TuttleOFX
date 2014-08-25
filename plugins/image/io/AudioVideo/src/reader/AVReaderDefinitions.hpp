c39f996e18074049e78b68f2e0cd02dfa5bec1af
Clement Champetier <cnt@mikrosimage.eu>
2014-08-25 13:42:54 +0200

    AVReaderDefinition: update name of OFX parameters
    * Use prefix for all OFX parameters.
    * The names are:
         * shorter
         * clearer (no need to know if the parameter comes from an FFmpeg
    option or not).

--- plugins/image/io/AudioVideo/src/reader/AVReaderDefinitions.hpp
+++ plugins/image/io/AudioVideo/src/reader/AVReaderDefinitions.hpp
@@ -9,26 +9,26 @@ namespace plugin {
 namespace av {
 namespace reader {
 
-static const std::string kParamFormatGroup               = "formatGroup";
-static const std::string kParamFormatDetailledGroup      = "formatDetailledGroup";
-static const std::string kParamVideoGroup                = "videoGroup";
-static const std::string kParamVideoDetailledGroup       = "videoDetailledGroup";
-static const std::string kParamMetaGroup                 = "metaGroup";
-static const std::string kParamMetaDetailledGroup        = "metaDetailledGroup";
+static const std::string kParamFormatGroup               = "f_group";
+static const std::string kParamFormatDetailledGroup      = "f_detailledGroup";
+static const std::string kParamVideoGroup                = "v_group";
+static const std::string kParamVideoDetailledGroup       = "v_detailledGroup";
+static const std::string kParamMetaGroup                 = "m_group";
+static const std::string kParamMetaDetailledGroup        = "m_detailledGroup";
 
-static const std::string kParamUseCustomSAR = "useCustomSAR";
-static const std::string kParamCustomSAR = "customSAR";
+static const std::string kParamUseCustomSAR = "v_useCustomSAR";
+static const std::string kParamCustomSAR = "v_customSAR";
 
-static const std::string kParamVideoStreamIndex      = "videoStreamIndex";
+static const std::string kParamVideoStreamIndex      = "v_streamIndex";
 static const std::string kParamVideoStreamIndexLabel = "Video stream index";
 
-static const std::string kParamMetaDataWrapper = "metaDataWrapper";
-static const std::string kParamMetaDataVideo = "metaDataVideo";
-static const std::string kParamMetaDataAudio = "metaDataAudio";
-static const std::string kParamMetaDataData = "metaDataData";
-static const std::string kParamMetaDataSubtitle = "metaDataSubtitle";
-static const std::string kParamMetaDataAttachement = "metaDataAttachement";
-static const std::string kParamMetaDataUnknown = "metaDataUnknown";
+static const std::string kParamMetaDataWrapper = "m_wrapper";
+static const std::string kParamMetaDataVideo = "m_video";
+static const std::string kParamMetaDataAudio = "m_audio";
+static const std::string kParamMetaDataData = "m_data";
+static const std::string kParamMetaDataSubtitle = "m_subtitle";
+static const std::string kParamMetaDataAttachement = "m_attachement";
+static const std::string kParamMetaDataUnknown = "m_unknown";
 
 }
 }
