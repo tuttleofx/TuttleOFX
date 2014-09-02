33c0bf81fdaa542b4d265fab458d034ef47dfbef
Cyril PICHARD <pih@devpc17.mikros.int>
2014-09-02 11:30:10 +0200

    adds boost_regex dependency to sequenceParser

--- cmake/FindsequenceParser.cmake
+++ cmake/FindsequenceParser.cmake
@@ -11,7 +11,7 @@ else(NOT EXISTS ${PROJECT_SOURCE_DIR}/libraries/sequenceParser/src)
     set(SEQUENCEPARSER_INCLUDE_DIR ${PROJECT_SOURCE_DIR}/libraries/sequenceParser/src)
     file(GLOB_RECURSE SEQUENCEPARSER_FILES ${PROJECT_SOURCE_DIR}/libraries/sequenceParser/src/*.?pp)
     set(SEQUENCEPARSER_FOUND 1)
-    set(SEQUENCEPARSER_LIBRARIES sequenceParser)
+    set(SEQUENCEPARSER_LIBRARIES sequenceParser boost_regex)
 
 endif(NOT EXISTS ${PROJECT_SOURCE_DIR}/libraries/sequenceParser/src)
       
