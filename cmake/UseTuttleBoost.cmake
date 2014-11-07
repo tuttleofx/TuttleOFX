43c914e4acfebb5d4fdbfeea8556269ad8c7ef2a
Fabien Castan <faca@mikrosimage.eu>
2014-11-07 21:11:27 +0100

    cmake: don't modify default boost_use_multithread option

--- cmake/UseTuttleBoost.cmake
+++ cmake/UseTuttleBoost.cmake
@@ -2,7 +2,6 @@
 # Boost for the whole Tuttle project
 
 set(Boost_USE_STATIC_LIBS OFF)
-set(Boost_USE_MULTITHREADED OFF) 
 find_package(Boost 1.53.0 
     COMPONENTS regex date_time chrono thread serialization system filesystem atomic log program_options timer QUIET)
 
