983ff994a2c66857d3325597d304262e216d4b66
Fabien Castan <fabien.castan@free.fr>
2014-11-17 03:00:05 +0100

    cmake: add BOOST_LOG_DYN_LINK flag

--- cmake/UseTuttleBoost.cmake
+++ cmake/UseTuttleBoost.cmake
@@ -2,6 +2,7 @@
 # Boost for the whole Tuttle project
 
 set(Boost_USE_STATIC_LIBS OFF)
+add_definitions(-DBOOST_LOG_DYN_LINK)
 find_package(Boost 1.53.0 
     COMPONENTS regex date_time chrono thread serialization system filesystem atomic log program_options timer QUIET)
 
@@ -10,3 +11,4 @@ if (Boost_FOUND)
 else(Boost_FOUND)
     message("please set BOOST_ROOT environment variable to a proper boost install")
 endif(Boost_FOUND)
+
