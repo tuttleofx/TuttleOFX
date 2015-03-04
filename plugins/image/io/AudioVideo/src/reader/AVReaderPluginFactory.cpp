3b02e99ad276aee12093ce18137b07aaac292c69
Clement Champetier <cnt@mikrosimage.eu>
2015-03-04 14:21:47 +0100

    AudioVideo: update avTranscoder to v0.5.3

--- plugins/image/io/AudioVideo/src/reader/AVReaderPluginFactory.cpp
+++ plugins/image/io/AudioVideo/src/reader/AVReaderPluginFactory.cpp
@@ -30,7 +30,7 @@ namespace reader {
 void AVReaderPluginFactory::describe( OFX::ImageEffectDescriptor& desc )
 {
 	avtranscoder::preloadCodecsAndFormats();
-	avtranscoder::setLogLevel( AV_LOG_QUIET );
+	avtranscoder::Logger::setLogLevel( AV_LOG_QUIET );
 
 	desc.setLabels(
 		"TuttleAVReader",
