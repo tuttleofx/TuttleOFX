34e550c2c1350cdb90f2251ceabd7870c5ed8cff
Clement Champetier <cnt@mikrosimage.eu>
2015-07-17 15:22:54 +0200

    CMake: update path to install python bindings

--- tools/travis/launch_tests.sh
+++ tools/travis/launch_tests.sh
@@ -3,7 +3,7 @@
 # set env
 export OFX_PLUGIN_PATH=${TUTTLE_INSTALL}/OFX
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${TUTTLE_INSTALL}/lib
-export PYTHONPATH=${TUTTLE_INSTALL}/lib/python2.7.3/site-packages:$PYTHONPATH
+export PYTHONPATH=${TUTTLE_INSTALL}/lib/python2.7/site-packages:$PYTHONPATH
 
 # clone data for tests
 git clone https://github.com/tuttleofx/TuttleOFX-data.git
