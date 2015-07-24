f1071152588747760bbda2006571704badd6017e
Fabien Castan <fabien.castan@free.fr>
2015-07-24 21:23:58 +0200

    pyTest: fix tab/space

--- libraries/tuttle/pyTest/graph/testFps.py
+++ libraries/tuttle/pyTest/graph/testFps.py
@@ -4,20 +4,20 @@ from nose.tools import *
 
 
 def setUp():
-	tuttle.core().preload(False)
+    tuttle.core().preload(False)
 
 
 def testFpsPropagation():
-        g = tuttle.Graph()
+    g = tuttle.Graph()
 
-        reader = g.createNode("tuttle.avreader", filename="TuttleOFX-data/video/morgen-20030714.avi")
-        inv1 = g.createNode("tuttle.invert")
-        inv2 = g.createNode("tuttle.invert")
-        writer = g.createNode("tuttle.avwriter", filename=".tests/output.avi")
+    reader = g.createNode("tuttle.avreader", filename="TuttleOFX-data/video/morgen-20030714.avi")
+    inv1 = g.createNode("tuttle.invert")
+    inv2 = g.createNode("tuttle.invert")
+    writer = g.createNode("tuttle.avwriter", filename=".tests/output.avi")
 
-	g.connect(reader, inv1, inv2, writer)
+    g.connect(reader, inv1, inv2, writer)
 
-	clipReader = reader.getOutputClip()
-	clipWriter = writer.getOutputClip()
+    clipReader = reader.getOutputClip()
+    clipWriter = writer.getOutputClip()
 
-        assert_equals(clipReader.getFrameRate(), clipWriter.getFrameRate())
+    assert_equals(clipReader.getFrameRate(), clipWriter.getFrameRate())
--- libraries/tuttle/pyTest/param/testParamProps.py
+++ libraries/tuttle/pyTest/param/testParamProps.py
@@ -2,19 +2,19 @@ from pyTuttle import tuttle
 
 
 def setUp():
-	tuttle.core().preload(False)
+    tuttle.core().preload(False)
 
 
 def testDoubleParamProps():
-	g = tuttle.Graph()
-	blur = g.createNode( "tuttle.blur", size={1.0:[80.0, 40.0], 9.0:[0.0, 0.0]} )
-	
-	blurSize = blur.getParam("size")
-        
-        # You can also access all OFX properties directly
-	print("Hint:", blurSize.getProperties().getStringProperty("OfxParamPropHint"))
+    g = tuttle.Graph()
+    blur = g.createNode( "tuttle.blur", size={1.0:[80.0, 40.0], 9.0:[0.0, 0.0]} )
 
-        # Print all properties
-        for p in blurSize.getProperties():
-            print(p.getType(), p.getName(), p.getDimension(), p.getStringValue())
+    blurSize = blur.getParam("size")
+
+    # You can also access all OFX properties directly
+    print("Hint:", blurSize.getProperties().getStringProperty("OfxParamPropHint"))
+
+    # Print all properties
+    for p in blurSize.getProperties():
+        print(p.getType(), p.getName(), p.getDimension(), p.getStringValue())
 
