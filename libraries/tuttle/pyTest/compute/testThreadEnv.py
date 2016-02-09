87cac767e44dbb364346d751f85d08ede2d46d8c
Clement Champetier <cnt@mikrosimage.eu>
2016-02-09 11:05:25 +0100

    pyTest: launch testThreadEnv with an invert node
    * The problems seems to appear with the blur node...
    * Issue #512

--- libraries/tuttle/pyTest/compute/testThreadEnv.py
+++ libraries/tuttle/pyTest/compute/testThreadEnv.py
@@ -1,16 +1,14 @@
 from pyTuttle import tuttle
-from nose.tools import nottest
 
 
 def setUp():
 	tuttle.core().preload(False)
 
 
-@nottest
 def testThreadEnv():
 	g = tuttle.Graph()
 	pngRead = g.createNode("tuttle.checkerboard", size=[50,50])
-	blur = g.createNode('tuttle.blur')
+	blur = g.createNode('tuttle.invert')
 	pngW = g.createNode('tuttle.pngwriter', ".tests/output-###.png")
 	g.connect( [pngRead, blur, pngW] )
 
