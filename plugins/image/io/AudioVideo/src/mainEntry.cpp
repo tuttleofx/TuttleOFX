3400c970219b2d31dec6e22e8904670c955e5167
Clement Champetier <cnt@mikrosimage.eu>
2016-04-15 13:00:55 +0200

    AudioVideo: up to v4.7

--- plugins/image/io/AudioVideo/src/mainEntry.cpp
+++ plugins/image/io/AudioVideo/src/mainEntry.cpp
@@ -1,5 +1,5 @@
 #define OFXPLUGIN_VERSION_MAJOR 4
-#define OFXPLUGIN_VERSION_MINOR 6
+#define OFXPLUGIN_VERSION_MINOR 7
 
 #include <tuttle/plugin/Plugin.hpp>
 #include "reader/AVReaderPluginFactory.hpp"
