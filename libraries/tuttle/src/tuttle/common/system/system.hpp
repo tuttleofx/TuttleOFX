f469fb927a791d8f58187112c1af48041fad40c3
Clement Champetier <cnt@mikrosimage.eu>
2016-04-18 17:13:43 +0200

    common: added preprocessor directive to define __UNIX__
    This define is used in the host Preferences to get the path to the
    plugin cache.

--- libraries/tuttle/src/tuttle/common/system/system.hpp
+++ libraries/tuttle/src/tuttle/common/system/system.hpp
@@ -7,11 +7,19 @@
 #define __LINUX__
 #endif
 
+#ifndef __UNIX__
+#define __UNIX__
+#endif
+
 #elif defined(macintosh) || defined(Macintosh) || defined(__APPLE__) || defined(__MACH__) || defined(MACOS) ||              \
     defined(MACOSX) || defined(__MACOS__)
 
 #ifndef __MACOS__
 #define __MACOS__
+#endif
+
+#ifndef __UNIX__
+#define __UNIX__
 #endif
 
 #elif defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(WIN64) || defined(_WIN64) || defined(__WIN64__) || \
