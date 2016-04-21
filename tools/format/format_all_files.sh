65720ce25203fad230c7b2c644fc8fd114337e29
Clement Champetier <cnt@mikrosimage.eu>
2016-04-21 10:35:35 +0200

    Format tools: refactored format_all_files.sh script
    No backslash needed.

--- tools/format/format_all_files.sh
+++ tools/format/format_all_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # libraries
-find ../../libraries -type f \( -iname \*.?pp \) -exec clang-format -i {} \;
+find ../../libraries -type f -iname "*.?pp" -exec clang-format -i {} \;
 # plugins
-find ../../plugins -type f \( -iname \*.?pp \) -exec clang-format -i {} \;
+find ../../plugins -type f -iname "*.?pp" -exec clang-format -i {} \;
 # applications
-find ../../applications -type f \( -iname \*.?pp \) -exec clang-format -i {} \;
+find ../../applications -type f -iname "*.?pp" -exec clang-format -i {} \;
