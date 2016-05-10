b2e5eb0e0678488c0b18d2b383b363cb078863c8
Clement Champetier <cnt@mikrosimage.eu>
2016-05-10 18:30:01 +0200

    oiio: up to v1.3

--- plugins/image/io/OpenImageIO/src/mainEntry.cpp
+++ plugins/image/io/OpenImageIO/src/mainEntry.cpp
@@ -1,5 +1,5 @@
 #define OFXPLUGIN_VERSION_MAJOR 1
-#define OFXPLUGIN_VERSION_MINOR 2
+#define OFXPLUGIN_VERSION_MINOR 3
 
 #include <tuttle/plugin/Plugin.hpp>
 #include "reader/OpenImageIOReaderPluginFactory.hpp"
