6cfbac7f8ec3ffd48b360a9d199291a09a443db1
Clement Champetier <cnt@mikrosimage.eu>
2016-06-07 14:28:10 +0200

    Travis: fixed import of numpy

--- tools/travis/install_dependencies.sh
+++ tools/travis/install_dependencies.sh
@@ -10,9 +10,11 @@ if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
     # Install python packages to run sam command line
     if [[ ${PYTHON_VERSION} == "2.7" ]]; then
         pip install --user clint argcomplete
-    # elif [[ ${PYTHON_VERSION} == "3.2" ]]; then
+        pip install --user numpy
+    elif [[ ${PYTHON_VERSION} == "3.2" ]]; then
         # easy_install3 pip --prefix=$HOME/local
         # pip3 install --user clint argcomplete
+        pip3 install --user numpy
     fi
 
     # Use TRAVIS_JOB_ID to detect that we are in travis.
