03ad407823fe8415615e71f37538826bfa5b9407
Clement Champetier <cnt@mikrosimage.eu>
2017-04-20 14:36:22 +0200

    Up to v0.13.6

--- libraries/tuttle/src/tuttle/host/version.hpp
+++ libraries/tuttle/src/tuttle/host/version.hpp
@@ -4,7 +4,7 @@
 // This is more than the version of the host: this is the version of the 'tuttle bundle' (host + plugins + sam).
 #define TUTTLE_HOST_VERSION_MAJOR 0
 #define TUTTLE_HOST_VERSION_MINOR 13
-#define TUTTLE_HOST_VERSION_MICRO 5
+#define TUTTLE_HOST_VERSION_MICRO 6
 
 #define TUTTLE_HOST_VERSION_STR                                                                                             \
     BOOST_PP_STRINGIZE(TUTTLE_HOST_VERSION_MAJOR) "." BOOST_PP_STRINGIZE(TUTTLE_HOST_VERSION_MINOR) "." BOOST_PP_STRINGIZE(TUTTLE_HOST_VERSION_MICRO)
